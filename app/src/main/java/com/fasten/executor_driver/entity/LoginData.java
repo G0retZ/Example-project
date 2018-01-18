7946bad43ca2aba920dc64bc1130689d530ef5d7
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-01-18 15:09:31 +0300

    Бизнес сущность для входа

---
+++ app/src/main/java/com/fasten/executor_driver/entity/LoginData.java
@@ -0,0 +1,68 @@
+package com.fasten.executor_driver.entity;
+
+import android.support.annotation.NonNull;
+
+/**
+ * Бизнес сущность входа в систему. Immutable.
+ * Создается через конструктор с не нулевыми полями.
+ * Любой сеттер возвращает новую сущность с заданными полями
+ */
+
+class LoginData {
+
+	@NonNull
+	private String login;
+	@NonNull
+	private String password;
+
+	LoginData(@NonNull String login, @NonNull String password) {
+		this.login = login;
+		this.password = password;
+	}
+
+	@NonNull
+	String getLogin() {
+		return login;
+	}
+
+	@NonNull
+	String getPassword() {
+		return password;
+	}
+
+    LoginData setLogin(@NonNull String login) {
+        return new LoginData(login, password);
+    }
+
+    LoginData setPassword(@NonNull String password) {
+        return new LoginData(login, password);
+    }
+
+    @Override
+	public String toString() {
+		return "LoginData{" +
+				"login='" + login + '\'' +
+				", password='" + password + '\'' +
+				'}';
+	}
+
+	@SuppressWarnings("SimplifiableIfStatement")
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		LoginData loginData = (LoginData) o;
+
+		if (!login.equals(loginData.login)) return false;
+		return password.equals(loginData.password);
+	}
+
+	@Override
+	public int hashCode() {
+		int result = login.hashCode();
+		result = 31 * result + password.hashCode();
+		return result;
+	}
+
+}
---
+++ app/src/test/java/com/fasten/executor_driver/entity/LoginDataTest.java
@@ -0,0 +1,41 @@
+package com.fasten.executor_driver.entity;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class LoginDataTest {
+
+    private LoginData loginData;
+
+    @Before
+    public void setUp() throws Exception {
+        loginData = new LoginData("name", "password");
+    }
+
+    @Test
+    public void testConstructor() throws Exception {
+        assertEquals(loginData.getLogin(), "name");
+        assertEquals(loginData.getPassword(), "password");
+    }
+
+    @Test
+    public void testSetters() throws Exception {
+        assertEquals(loginData.setLogin("nam").getLogin(), "nam");
+        assertEquals(loginData.setLogin("nam").getLogin(), "nam");
+        assertEquals(loginData.setPassword("pass").getPassword(), "pass");
+        assertEquals(loginData.setPassword("pass").getPassword(), "pass");
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+        assertEquals(loginData, new LoginData("name", "password"));
+        assertNotEquals(loginData, new LoginData("nam", "password"));
+        assertNotEquals(loginData, new LoginData("name", "passwor"));
+        assertNotEquals(loginData, loginData.setLogin("namee"));
+        assertNotEquals(loginData, loginData.setPassword("passwordd"));
+    }
+
+}
