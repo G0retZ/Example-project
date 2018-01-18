83f5bf2d1f6bf7a5856badc41afaf3063f157f21
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-01-18 12:38:22 +0300

    основные файлы апп

---
+++ app/src/main/java/com/fasten/executor_driver/application/MainActivity.java
@@ -0,0 +1,7 @@
+package com.fasten.executor_driver.application;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/fasten/executor_driver/application/MainApplication.java
@@ -0,0 +1,16 @@
+package com.fasten.executor_driver.application;
+
+import android.app.Application;
+
+/**
+ * Application.
+ */
+
+public class MainApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+
+}
