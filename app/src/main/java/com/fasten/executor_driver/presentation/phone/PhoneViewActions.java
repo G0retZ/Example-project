f32263292bb08e5681e1268c714134bb1b7cdbe0
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-01-25 18:55:39 +0300

    feat: действия над видом и его ViewModel

---
+++ app/src/main/java/com/fasten/executor_driver/presentation/phone/PhoneViewActions.java
@@ -0,0 +1,34 @@
+package com.fasten.executor_driver.presentation.phone;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Действия для смены состояния вида окна входа
+ */
+public interface PhoneViewActions {
+	/**
+	 * Перейти на следующий шаг. Передает проверенный логин, чтобы сохранился в бандле, и переживал
+	 * прибитие апп
+	 * @param login - логин для входа
+	 */
+	void proceedNext(@NonNull String login);
+
+	/**
+	 * Сделать кнопку "Далее" нажимаемой
+	 * @param enable - нажимаема или нет?
+	 */
+	void enableButton(boolean enable);
+
+	/**
+	 * Показать ошибку
+	 * @param error - ошибка
+	 */
+	void showError(@Nullable Throwable error);
+
+	/**
+	 * Показать инликатор процесса
+	 * @param pending - показать или нет?
+	 */
+	void showPending(boolean pending);
+}
