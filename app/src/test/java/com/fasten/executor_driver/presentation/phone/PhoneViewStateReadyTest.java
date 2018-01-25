dc11f6e17f87950bb1e46a9f7fd24ed0cff98cc6
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-01-25 18:56:06 +0300

    feat: все состояния вида с тестами

---
+++ app/src/test/java/com/fasten/executor_driver/presentation/phone/PhoneViewStateReadyTest.java
@@ -0,0 +1,37 @@
+package com.fasten.executor_driver.presentation.phone;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PhoneViewStateReadyTest {
+
+	private PhoneViewStateReady viewState;
+
+	@Mock
+	private PhoneViewActions phoneViewActions;
+
+	@Before
+	public void setUp() throws Exception {
+		viewState = new PhoneViewStateReady();
+	}
+
+	@Test
+	public void testActions() throws Exception {
+		// when:
+		viewState.apply(phoneViewActions);
+
+		// then:
+		verify(phoneViewActions).showPending(false);
+		verify(phoneViewActions).showError(null);
+		verify(phoneViewActions).enableButton(true);
+		verifyNoMoreInteractions(phoneViewActions);
+	}
+
+}
