3ba9ce04b464fdef40f732981e429320103941a8
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-01-26 13:22:09 +0300

    test: тест гейтвея запроса звонка с кодом

---
+++ app/src/test/java/com/fasten/executor_driver/interactor/auth/PhoneCallGatewayTest.java
@@ -0,0 +1,83 @@
+package com.fasten.executor_driver.interactor.auth;
+
+import com.fasten.executor_driver.backend.web.ApiService;
+import com.fasten.executor_driver.backend.web.NoNetworkException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import io.reactivex.Completable;
+import io.reactivex.plugins.RxJavaPlugins;
+import io.reactivex.schedulers.Schedulers;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PhoneCallGatewayTest {
+
+	private PhoneCallGateway phoneCallGateway;
+
+	@Mock
+	private ApiService api;
+
+	@Before
+	public void setUp() throws Exception {
+		RxJavaPlugins.setIoSchedulerHandler(scheduler -> Schedulers.trampoline());
+		RxJavaPlugins.setSingleSchedulerHandler(scheduler -> Schedulers.trampoline());
+		when(api.callMeCode(anyString())).thenReturn(Completable.never());
+	}
+
+	/* Проверяем работу с АПИ */
+
+	/**
+	 * Должен запросить у АПИ completable на вход с заданными параметрами
+	 *
+	 * @throws Exception error
+	 */
+	@Test
+	public void authCompletableRequested() throws Exception {
+		// when:
+		phoneCallGateway.callMe("012345");
+
+		// then:
+		verify(api, only()).callMeCode("012345");
+	}
+
+	/* Проверяем правильность потоков (добавить) */
+
+	/* Проверяем ответы на АПИ */
+
+	/**
+	 * Должен ответить ошибкой сети
+	 *
+	 * @throws Exception error
+	 */
+	@Test
+	public void answerNoNetworkError() throws Exception {
+		// when:
+		when(api.callMeCode(anyString())).thenReturn(Completable.error(new NoNetworkException()));
+
+		// then:
+		phoneCallGateway.callMe("01234").test().assertError(NoNetworkException.class);
+	}
+
+	/**
+	 * Должен ответить успехом
+	 *
+	 * @throws Exception error
+	 */
+	@Test
+	public void answerLoginSuccessful() throws Exception {
+		// when:
+		when(api.callMeCode(anyString())).thenReturn(Completable.complete());
+
+		// then:
+		phoneCallGateway.callMe("012345").test().assertComplete();
+	}
+}
