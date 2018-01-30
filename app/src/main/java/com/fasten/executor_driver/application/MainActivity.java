da40fbc519fe20ed80f48c1ec7036d43cbae64c8
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-01-30 16:08:53 +0300

    feat: Главная активити с проверкой токена и переходом к авторизации

--- app/src/main/java/com/fasten/executor_driver/application/MainActivity.java
+++ app/src/main/java/com/fasten/executor_driver/application/MainActivity.java
@@ -1,6 +1,32 @@
 package com.fasten.executor_driver.application;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
-public class MainActivity extends AppCompatActivity {
+import com.fasten.executor_driver.R;
+import com.fasten.executor_driver.backend.web.TokenKeeper;
+
+import javax.inject.Inject;
+
+public class MainActivity extends BaseActivity {
+
+	private TokenKeeper tokenKeeper;
+
+	@Inject
+	public void setTokenKeeper(TokenKeeper tokenKeeper) {
+		this.tokenKeeper = tokenKeeper;
+	}
+
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_auth);
+		getDiComponent().inject(this);
+		if (tokenKeeper.getToken() == null) {
+			startActivity(new Intent(this, AuthActivity.class));
+			finish();
+		}
+	}
+
 }
--- app/src/main/java/com/fasten/executor_driver/di/AppComponent.java
+++ app/src/main/java/com/fasten/executor_driver/di/AppComponent.java
@@ -1,5 +1,6 @@
 package com.fasten.executor_driver.di;
 
+import com.fasten.executor_driver.application.MainActivity;
 import com.fasten.executor_driver.view.auth.LoginFragment;
 
 import javax.inject.Singleton;
@@ -10,4 +11,5 @@ import dagger.Component;
 @Component(modules = {AppModule.class})
 public interface AppComponent {
 	void inject(LoginFragment loginFragment);
+	void inject(MainActivity mainActivity);
 }
