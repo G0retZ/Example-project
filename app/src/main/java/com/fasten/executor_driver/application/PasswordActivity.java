c88ae6819b44dfc14bd7bcd35341b71df9eb0af7
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-02-01 17:46:45 +0300

    feat: Активити для ввода кода с лайаутом

--- app/src/main/java/com/fasten/executor_driver/application/PasswordActivity.java
+++ app/src/main/java/com/fasten/executor_driver/application/PasswordActivity.java
@@ -4,22 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-
 import com.fasten.executor_driver.R;
 
-public class LoginActivity extends BaseActivity {
+public class PasswordActivity extends BaseActivity {
 
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_auth);
-    setTitle(R.string.authorization);
+    setContentView(R.layout.activity_passwrod);
+    setTitle(R.string.enter);
   }
 
   @Override
   public void navigate(@NonNull String destination) {
-    if (destination.equals("next")) {
-      startActivity(new Intent(this, PasswordActivity.class));
+    if (destination.equals("enter")) {
+      Intent intent = new Intent(this, MainActivity.class);
+      intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+      startActivity(intent);
+      finish();
     }
   }
 }
