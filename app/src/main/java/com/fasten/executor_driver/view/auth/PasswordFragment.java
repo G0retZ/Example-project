8ac80cffab8473211bc0dbb402c30837205f2869
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-02-01 17:46:12 +0300

    feat: Фрагмент для ввода кода с лайаутом

---
+++ app/src/main/java/com/fasten/executor_driver/view/auth/PasswordFragment.java
@@ -0,0 +1,136 @@
+package com.fasten.executor_driver.view.auth;
+
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProvider.Factory;
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import com.fasten.executor_driver.R;
+import com.fasten.executor_driver.backend.web.NoNetworkException;
+import com.fasten.executor_driver.di.AppComponent;
+import com.fasten.executor_driver.presentation.code.CodeViewActions;
+import com.fasten.executor_driver.presentation.code.CodeViewModel;
+import com.fasten.executor_driver.presentation.code.CodeViewModelImpl;
+import com.fasten.executor_driver.presentation.timeoutbutton.TimeoutButtonViewActions;
+import com.fasten.executor_driver.presentation.timeoutbutton.TimeoutButtonViewModel;
+import com.fasten.executor_driver.presentation.timeoutbutton.TimeoutButtonViewModelImpl;
+import com.fasten.executor_driver.view.BaseFragment;
+import com.jakewharton.rxbinding2.widget.RxTextView;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ * Отображает поле для ввода логина.
+ */
+
+public class PasswordFragment extends BaseFragment implements CodeViewActions,
+    TimeoutButtonViewActions {
+
+  private CodeViewModel codeViewModel;
+  private TimeoutButtonViewModel timeoutButtonViewModel;
+  private TextInputLayout codeInputLayout;
+  private TextInputEditText codeInput;
+  private Button sendSms;
+  private ProgressBar pendingIndicator;
+  private final OnClickListener sendSmsClickListener = v -> {
+    if (timeoutButtonViewModel.buttonClicked()) {
+      codeViewModel.sendMeSms();
+    }
+  };
+
+  private ViewModelProvider.Factory codeViewModelFactory;
+  private ViewModelProvider.Factory buttonViewModelFactory;
+
+  @Inject
+  public void setCodeViewModelFactory(@Named("code") Factory codeViewModelFactory) {
+    this.codeViewModelFactory = codeViewModelFactory;
+  }
+
+  @Inject
+  public void setButtonViewModelFactory(@Named("button") Factory buttonViewModelFactory) {
+    this.buttonViewModelFactory = buttonViewModelFactory;
+  }
+
+  @Override
+  protected void onDependencyInject(AppComponent appComponent) {
+    // Required by Dagger2 for field injection
+    appComponent.inject(this);
+    codeViewModel = ViewModelProviders.of(this, codeViewModelFactory).get(CodeViewModelImpl.class);
+    timeoutButtonViewModel = ViewModelProviders.of(this, buttonViewModelFactory)
+        .get(TimeoutButtonViewModelImpl.class);
+  }
+
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater,
+      @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.fragment_auth_password, container, false);
+    codeInputLayout = view.findViewById(R.id.codeInputLayout);
+    codeInput = view.findViewById(R.id.codeInput);
+    sendSms = view.findViewById(R.id.sendSms);
+    pendingIndicator = view.findViewById(R.id.pending);
+
+    codeViewModel.getViewStateLiveData().observe(this, viewState -> {
+      if (viewState != null) {
+        viewState.apply(this);
+      }
+    });
+    setTextListener();
+    if (savedInstanceState == null) {
+      sendSms.performClick();
+    }
+    return view;
+  }
+
+  @Override
+  public void showPending(boolean pending) {
+    pendingIndicator.setVisibility(pending ? View.VISIBLE : View.GONE);
+  }
+
+  @Override
+  public void letIn() {
+    navigate("enter");
+  }
+
+  @Override
+  public void showError(@Nullable Throwable error) {
+    if (error == null) {
+      codeInputLayout.setError(null);
+    } else {
+      if (error instanceof NoNetworkException) {
+        codeInputLayout.setError(getString(R.string.no_network_connection));
+      } else {
+        codeInputLayout.setError(getString(R.string.invalid_code));
+      }
+    }
+  }
+
+  // Замудренная логика форматировния ввода номера телефона в режиме реального времени
+  private void setTextListener() {
+    RxTextView.textChanges(codeInput).subscribe(code -> codeViewModel.setCode(code.toString()));
+  }
+
+  @Override
+  public void showTimer(@Nullable Long secondsLeft) {
+    if (secondsLeft == null) {
+      sendSms.setText(R.string.get_code_from_sms);
+    } else {
+      sendSms.setText(getString(R.string.repeat_code_from_sms, secondsLeft));
+    }
+  }
+
+  @Override
+  public void setResponsive(boolean responsive) {
+    sendSms.setOnClickListener(responsive ? sendSmsClickListener : null);
+  }
+}
