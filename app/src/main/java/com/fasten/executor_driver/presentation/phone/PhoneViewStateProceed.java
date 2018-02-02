f7bb427bdf3272dc3975c4cbce26322790bc8f57
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-02-02 11:40:05 +0300

    fix: поправил возможность редактировать номер телефона при возврате назад.

--- app/src/main/java/com/fasten/executor_driver/presentation/phone/PhoneViewStateProceed.java
+++ app/src/main/java/com/fasten/executor_driver/presentation/phone/PhoneViewStateProceed.java
@@ -26,7 +26,7 @@ public final class PhoneViewStateProceed implements ViewState<PhoneViewActions> 
   public void apply(@NonNull PhoneViewActions stateActions) {
     stateActions.showPending(false);
     stateActions.showError(null);
-    stateActions.setInputEditable(false);
+    stateActions.setInputEditable(true);
     stateActions.enableButton(false);
     stateActions.proceedNext(login);
   }
