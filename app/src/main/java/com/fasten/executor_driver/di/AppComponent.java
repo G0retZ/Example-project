93218de266adeb11f79506b09a84352afcbb7fbe
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-02-08 17:44:03 +0300

    feat: вид кнопки выхода на линию

--- app/src/main/java/com/fasten/executor_driver/di/AppComponent.java
+++ app/src/main/java/com/fasten/executor_driver/di/AppComponent.java
@@ -1,5 +1,6 @@
 package com.fasten.executor_driver.di;
 
+import com.fasten.executor_driver.view.GoOnlineFragment;
 import com.fasten.executor_driver.view.MapFragment;
 import com.fasten.executor_driver.view.auth.LoginFragment;
 import com.fasten.executor_driver.view.auth.PasswordFragment;
@@ -21,4 +22,6 @@ public interface AppComponent {
   void inject(PasswordFragment loginFragment);
 
   void inject(MapFragment mapFragment);
+
+  void inject(GoOnlineFragment mapFragment);
 }
