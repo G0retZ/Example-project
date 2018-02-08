55e6047e692d49c268b16626e70bd2d0ef169545
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-02-08 12:02:57 +0300

    fix: поправил ошибки линта, сделав пустой тест не совсем пустым

--- app/src/test/java/com/fasten/executor_driver/interactor/map/HeatMapGatewayTest.java
+++ app/src/test/java/com/fasten/executor_driver/interactor/map/HeatMapGatewayTest.java
@@ -1,5 +1,15 @@
 package com.fasten.executor_driver.interactor.map;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class HeatMapGatewayTest {
+
+  @Test
+  public void authCompletableRequested() throws Exception {
+    System.out.println("Stub!");
+  }
 
 }
