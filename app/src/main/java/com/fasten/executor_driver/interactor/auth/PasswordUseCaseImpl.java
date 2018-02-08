3132cbb8b6de6106da0d9802cb27b10148ae3bef
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-02-08 17:38:24 +0300

    fix: поправил баги и форматирование

--- app/src/main/java/com/fasten/executor_driver/interactor/auth/PasswordUseCaseImpl.java
+++ app/src/main/java/com/fasten/executor_driver/interactor/auth/PasswordUseCaseImpl.java
@@ -2,8 +2,8 @@ package com.fasten.executor_driver.interactor.auth;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import com.fasten.executor_driver.entity.ValidationException;
 import com.fasten.executor_driver.entity.LoginData;
+import com.fasten.executor_driver.entity.ValidationException;
 import com.fasten.executor_driver.entity.Validator;
 import com.fasten.executor_driver.interactor.DataSharer;
 import io.reactivex.Completable;
