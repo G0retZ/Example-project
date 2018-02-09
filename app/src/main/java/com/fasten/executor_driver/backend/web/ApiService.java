631ff959572cd93a7c81ae85809acafea063c009
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-02-09 16:02:53 +0300

    feat: сделал реальный гейтвей с тестом

--- app/src/main/java/com/fasten/executor_driver/backend/web/ApiService.java
+++ app/src/main/java/com/fasten/executor_driver/backend/web/ApiService.java
@@ -3,6 +3,7 @@ package com.fasten.executor_driver.backend.web;
 import android.support.annotation.NonNull;
 import com.fasten.executor_driver.backend.web.model.ApiLogin;
 import io.reactivex.Completable;
+import io.reactivex.Single;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
@@ -16,12 +17,18 @@ public interface ApiService {
   /*
    *  Запрос СМС с кодом
    */
-  @GET("public/v1/login/password/sms")
+  @GET("api/public/v1/login/password/sms")
   Completable sendMeCode(@NonNull @Query("login") String phoneNumber);
 
   /*
    *  Авторизация
    */
-  @POST("public/v1/login")
+  @POST("api/public/v1/login")
   Completable authorize(@NonNull @Body ApiLogin apiLogin);
+
+  /*
+   *  Запрос тепловой карты
+   */
+  @GET("geoMap")
+  Single<String> getHeatMap();
 }
--- app/src/main/java/com/fasten/executor_driver/gateway/HeatMapGatewayImpl.java
+++ app/src/main/java/com/fasten/executor_driver/gateway/HeatMapGatewayImpl.java
@@ -1,46 +1,27 @@
 package com.fasten.executor_driver.gateway;
 
-import android.content.Context;
 import android.support.annotation.NonNull;
-import com.fasten.executor_driver.R;
+import com.fasten.executor_driver.backend.web.ApiService;
 import com.fasten.executor_driver.interactor.map.HeatMapGateway;
 import io.reactivex.Single;
 import io.reactivex.schedulers.Schedulers;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.StringWriter;
-import java.io.Writer;
 import javax.inject.Inject;
 
 public class HeatMapGatewayImpl implements HeatMapGateway {
 
-  private final int[] jsonFiles = new int[]{R.raw.ttk, R.raw.star, R.raw.mkad, R.raw.heart,
-      R.raw.key};
-  private final Context context;
-  private int counter;
+  @NonNull
+  private final ApiService api;
 
   @Inject
-  public HeatMapGatewayImpl(Context context) {
-    this.context = context;
+  public HeatMapGatewayImpl(@NonNull ApiService api) {
+    this.api = api;
   }
 
   @NonNull
   @Override
   public Single<String> getHeatMap() {
-    return Single.fromCallable(() -> {
-      Writer writer = new StringWriter();
-      char[] buffer = new char[1024];
-      try (InputStream is = context.getResources().openRawResource(jsonFiles[counter % 5])) {
-        Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-        int n;
-        while ((n = reader.read(buffer)) != -1) {
-          writer.write(buffer, 0, n);
-        }
-      }
-      counter++;
-      return writer.toString();
-    }).subscribeOn(Schedulers.io()).observeOn(Schedulers.single());
+    return api.getHeatMap()
+        .subscribeOn(Schedulers.io())
+        .observeOn(Schedulers.single());
   }
 }
