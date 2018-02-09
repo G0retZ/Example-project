908353a09a1b94b89950e79997e8647433cb4eb1
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-02-09 20:08:29 +0300

    fix: Чтобы retrofit распознавал примитивы.

--- app/src/main/java/com/fasten/executor_driver/di/AppModule.java
+++ app/src/main/java/com/fasten/executor_driver/di/AppModule.java
@@ -20,6 +20,7 @@ import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 @Module
 public class AppModule {
@@ -80,6 +81,7 @@ public class AppModule {
     return new Retrofit.Builder()
         .baseUrl(BuildConfig.BASE_URL)
         .client(builder.build())
+        .addConverterFactory(ScalarsConverterFactory.create())
         .addConverterFactory(GsonConverterFactory.create())
         .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .build()
