f194434699097e19da7411f4a2ac518055d452db
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-02-14 17:09:20 +0300

    feat: DI для преобрахователей

--- app/src/main/java/com/fasten/executor_driver/di/GatewayModule.java
+++ app/src/main/java/com/fasten/executor_driver/di/GatewayModule.java
@@ -2,17 +2,25 @@ package com.fasten.executor_driver.di;
 
 import android.support.annotation.NonNull;
 import com.fasten.executor_driver.backend.web.TokenKeeper;
+import com.fasten.executor_driver.backend.web.incoming.ApiVehicle;
+import com.fasten.executor_driver.backend.web.incoming.ApiVehicleOptionItem;
+import com.fasten.executor_driver.entity.Vehicle;
+import com.fasten.executor_driver.entity.VehicleOption;
 import com.fasten.executor_driver.gateway.HeatMapGatewayImpl;
+import com.fasten.executor_driver.gateway.Mapper;
 import com.fasten.executor_driver.gateway.OnlineGatewayImpl;
 import com.fasten.executor_driver.gateway.PasswordGatewayImpl;
 import com.fasten.executor_driver.gateway.SmsGatewayImpl;
 import com.fasten.executor_driver.gateway.TokenKeeperImpl;
+import com.fasten.executor_driver.gateway.VehicleApiMapper;
+import com.fasten.executor_driver.gateway.VehicleOptionApiMapper;
 import com.fasten.executor_driver.interactor.auth.PasswordGateway;
 import com.fasten.executor_driver.interactor.auth.SmsGateway;
 import com.fasten.executor_driver.interactor.map.HeatMapGateway;
 import com.fasten.executor_driver.interactor.online.OnlineGateway;
 import dagger.Module;
 import dagger.Provides;
+import javax.inject.Named;
 
 @Module
 class GatewayModule {
@@ -41,5 +49,19 @@ class GatewayModule {
   @Provides
   OnlineGateway provideOnlineGateway(OnlineGatewayImpl onlineGateway) {
     return onlineGateway;
+  }
+
+  /* Преобразователи */
+
+  @Provides
+  @Named("vehicleOptionMapper")
+  Mapper<ApiVehicleOptionItem, VehicleOption> provideVehicleOptionApiMapper(VehicleOptionApiMapper vehicleOptionApiMapper) {
+    return vehicleOptionApiMapper;
+  }
+
+  @Provides
+  @Named("vehicleMapper")
+  Mapper<ApiVehicle, Vehicle> provideVehicleApiMapper(VehicleApiMapper vehicleApiMapper) {
+    return vehicleApiMapper;
   }
 }
