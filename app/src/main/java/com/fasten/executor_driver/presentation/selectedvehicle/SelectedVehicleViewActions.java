65c1768e1aaa4e879472e099d9164dfc7968a724
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-02-22 10:44:56 +0300

    feat: Модель состояния вида окна информации о выбранном ТС с тестами.

---
+++ app/src/main/java/com/fasten/executor_driver/presentation/selectedvehicle/SelectedVehicleViewActions.java
@@ -0,0 +1,21 @@
+package com.fasten.executor_driver.presentation.selectedvehicle;
+
+/**
+ * Действия для смены состояния вида окна информации о выбранном ТС.
+ */
+public interface SelectedVehicleViewActions {
+
+  /**
+   * Указать имя ТС.
+   *
+   * @param name - имя ТС
+   */
+  void setVehicleName(String name);
+
+  /**
+   * Сделать кнопку "Сменить" нажимаемой.
+   *
+   * @param enable - нажимаема или нет?
+   */
+  void enableChangeButton(boolean enable);
+}
