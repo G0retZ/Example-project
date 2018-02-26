6dccd670f4d268c9acbbf9efd9d1f39339b51c63
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-02-26 10:37:36 +0300

    feat: гамбургер на карте.

--- app/src/main/java/com/fasten/executor_driver/application/MapActivity.java
+++ app/src/main/java/com/fasten/executor_driver/application/MapActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import com.fasten.executor_driver.R;
 import com.fasten.executor_driver.presentation.onlinebutton.OnlineButtonNavigate;
 
@@ -14,6 +15,12 @@ public class MapActivity extends BaseActivity {
   public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_map);
+    ActionBar actionBar = getSupportActionBar();
+    if (actionBar != null) {
+      actionBar.setTitle("");
+      actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_24dp);
+      actionBar.setDisplayHomeAsUpEnabled(true);
+    }
   }
 
   @Override
