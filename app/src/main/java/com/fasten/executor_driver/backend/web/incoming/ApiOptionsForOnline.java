e06e3e243577fdabaca86f3e4f33adfd690421cb
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-03-01 13:31:24 +0300

    feat: модель ответа сервера с опциями исполнителя + опции ТС.

---
+++ app/src/main/java/com/fasten/executor_driver/backend/web/incoming/ApiOptionsForOnline.java
@@ -0,0 +1,69 @@
+package com.fasten.executor_driver.backend.web.incoming;
+
+import com.google.gson.annotations.SerializedName;
+import java.util.List;
+
+/**
+ * ответ от API содержащий список ТС и список опций водителя.
+ */
+public class ApiOptionsForOnline {
+
+  @SerializedName("cars")
+  private List<ApiVehicle> cars;
+  @SerializedName("driverOptions")
+  private List<ApiOptionItem> driverOptions;
+
+  /**
+   * Конструктор без параметров желателен для безопасной работы Gson.
+   */
+  @SuppressWarnings({"unused", "SpellCheckingInspection"})
+  public ApiOptionsForOnline() {
+  }
+
+  ApiOptionsForOnline(List<ApiVehicle> cars, List<ApiOptionItem> driverOptions) {
+    this.cars = cars;
+    this.driverOptions = driverOptions;
+  }
+
+  List<ApiVehicle> getCars() {
+    return cars;
+  }
+
+  List<ApiOptionItem> getDriverOptions() {
+    return driverOptions;
+  }
+
+  @Override
+  public String toString() {
+    return "ApiOptionsForOnline{" +
+        "cars=" + cars +
+        ", driverOptions=" + driverOptions +
+        '}';
+  }
+
+  @SuppressWarnings("SimplifiableIfStatement")
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    ApiOptionsForOnline that = (ApiOptionsForOnline) o;
+
+    if (cars != null ? !cars.equals(that.cars) : that.cars != null) {
+      return false;
+    }
+    return driverOptions != null ? driverOptions.equals(that.driverOptions)
+        : that.driverOptions == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = cars != null ? cars.hashCode() : 0;
+    result = 31 * result + (driverOptions != null ? driverOptions.hashCode() : 0);
+    return result;
+  }
+}
---
+++ app/src/test/java/com/fasten/executor_driver/backend/web/incoming/ApiOptionsForOnlineTest.java
@@ -0,0 +1,145 @@
+package com.fasten.executor_driver.backend.web.incoming;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ApiOptionsForOnlineTest {
+
+  private ApiOptionsForOnline apiOptionsForOnline;
+
+  @Before
+  public void setUp() throws Exception {
+    apiOptionsForOnline = new ApiOptionsForOnline(
+        Arrays.asList(
+            new ApiVehicle(0, "mark1", "model1", "lic1", "col1", false, new ArrayList<>()),
+            new ApiVehicle(1, "mark2", "model2", "lic2", "col2", false, new ArrayList<>()),
+            new ApiVehicle(2, "mark3", "model3", "lic3", "col3", false, new ArrayList<>())
+        ),
+        Arrays.asList(
+            new ApiOptionItem(324, "opt1", "desc1", true, false, "val1", -5, 3),
+            new ApiOptionItem(31, "opt2", "desc2", true, true, "val2", 5, 20),
+            new ApiOptionItem(523, "opt3", "desc3", false, true, "val3", null, null),
+            new ApiOptionItem(42, "opt4", "desc4", false, false, "val4", null, null)
+        )
+    );
+  }
+
+  @Test
+  public void testConstructor() throws Exception {
+    assertEquals(apiOptionsForOnline.getCars(), Arrays.asList(
+        new ApiVehicle(0, "mark1", "model1", "lic1", "col1", false, new ArrayList<>()),
+        new ApiVehicle(1, "mark2", "model2", "lic2", "col2", false, new ArrayList<>()),
+        new ApiVehicle(2, "mark3", "model3", "lic3", "col3", false, new ArrayList<>())
+    ));
+    assertEquals(apiOptionsForOnline.getDriverOptions(), Arrays.asList(
+            new ApiOptionItem(324, "opt1", "desc1", true, false, "val1", -5, 3),
+            new ApiOptionItem(31, "opt2", "desc2", true, true, "val2", 5, 20),
+            new ApiOptionItem(523, "opt3", "desc3", false, true, "val3", null, null),
+            new ApiOptionItem(42, "opt4", "desc4", false, false, "val4", null, null)
+    ));
+  }
+
+  @SuppressWarnings("SpellCheckingInspection")
+  @Test
+  public void testEquals() throws Exception {
+    assertEquals(apiOptionsForOnline,
+        new ApiOptionsForOnline(
+            Arrays.asList(
+                new ApiVehicle(0, "mark1", "model1", "lic1", "col1", false, new ArrayList<>()),
+                new ApiVehicle(1, "mark2", "model2", "lic2", "col2", false, new ArrayList<>()),
+                new ApiVehicle(2, "mark3", "model3", "lic3", "col3", false, new ArrayList<>())
+            ),
+            Arrays.asList(
+                new ApiOptionItem(324, "opt1", "desc1", true, false, "val1", -5, 3),
+                new ApiOptionItem(31, "opt2", "desc2", true, true, "val2", 5, 20),
+                new ApiOptionItem(523, "opt3", "desc3", false, true, "val3", null, null),
+                new ApiOptionItem(42, "opt4", "desc4", false, false, "val4", null, null)
+            )
+        )
+    );
+    assertNotEquals(apiOptionsForOnline,
+        new ApiOptionsForOnline(
+            new ArrayList<>(),
+            Arrays.asList(
+                new ApiOptionItem(324, "opt1", "desc1", true, false, "val1", -5, 3),
+                new ApiOptionItem(31, "opt2", "desc2", true, true, "val2", 5, 20),
+                new ApiOptionItem(523, "opt3", "desc3", false, true, "val3", null, null),
+                new ApiOptionItem(42, "opt4", "desc4", false, false, "val4", null, null)
+            )
+        )
+    );
+    assertNotEquals(apiOptionsForOnline,
+        new ApiOptionsForOnline(
+            Arrays.asList(
+                new ApiVehicle(0, "mark1", "model1", "lic1", "col1", false, new ArrayList<>()),
+                new ApiVehicle(1, "mark2", "model2", "lic2", "col2", false, new ArrayList<>())
+            ),
+            Arrays.asList(
+                new ApiOptionItem(324, "opt1", "desc1", true, false, "val1", -5, 3),
+                new ApiOptionItem(31, "opt2", "desc2", true, true, "val2", 5, 20),
+                new ApiOptionItem(523, "opt3", "desc3", false, true, "val3", null, null),
+                new ApiOptionItem(42, "opt4", "desc4", false, false, "val4", null, null)
+            )
+        )
+    );
+    assertNotEquals(apiOptionsForOnline,
+        new ApiOptionsForOnline(
+            Arrays.asList(
+                new ApiVehicle(0, "mark1", "model1", "lic1", "col1", false, new ArrayList<>()),
+                new ApiVehicle(1, "mark2", "model2", "lic2", "col2", false, new ArrayList<>()),
+                new ApiVehicle(3, "mark3", "model3", "lic3", "col3", false, new ArrayList<>())
+            ),
+            Arrays.asList(
+                new ApiOptionItem(324, "opt1", "desc1", true, false, "val1", -5, 3),
+                new ApiOptionItem(31, "opt2", "desc2", true, true, "val2", 5, 20),
+                new ApiOptionItem(523, "opt3", "desc3", false, true, "val3", null, null),
+                new ApiOptionItem(42, "opt4", "desc4", false, false, "val4", null, null)
+            )
+        )
+    );
+    assertNotEquals(apiOptionsForOnline,
+        new ApiOptionsForOnline(
+            Arrays.asList(
+                new ApiVehicle(0, "mark1", "model1", "lic1", "col1", false, new ArrayList<>()),
+                new ApiVehicle(1, "mark2", "model2", "lic2", "col2", false, new ArrayList<>()),
+                new ApiVehicle(2, "mark3", "model3", "lic3", "col3", false, new ArrayList<>())
+            ),
+            new ArrayList<>()
+        )
+    );
+    assertNotEquals(apiOptionsForOnline,
+        new ApiOptionsForOnline(
+            Arrays.asList(
+                new ApiVehicle(0, "mark1", "model1", "lic1", "col1", false, new ArrayList<>()),
+                new ApiVehicle(1, "mark2", "model2", "lic2", "col2", false, new ArrayList<>()),
+                new ApiVehicle(2, "mark3", "model3", "lic3", "col3", false, new ArrayList<>())
+            ),
+            Arrays.asList(
+                new ApiOptionItem(324, "opt1", "desc1", true, false, "val1", -5, 3),
+                new ApiOptionItem(523, "opt3", "desc3", false, true, "val3", null, null),
+                new ApiOptionItem(42, "opt4", "desc4", false, false, "val4", null, null)
+            )
+        )
+    );
+    assertNotEquals(apiOptionsForOnline,
+        new ApiOptionsForOnline(
+            Arrays.asList(
+                new ApiVehicle(0, "mark1", "model1", "lic1", "col1", false, new ArrayList<>()),
+                new ApiVehicle(1, "mark2", "model2", "lic2", "col2", false, new ArrayList<>()),
+                new ApiVehicle(2, "mark3", "model3", "lic3", "col3", false, new ArrayList<>())
+            ),
+            Arrays.asList(
+                new ApiOptionItem(32, "opt1", "desc1", true, false, "val1", -5, 3),
+                new ApiOptionItem(31, "opt2", "desc2", true, true, "val2", 5, 20),
+                new ApiOptionItem(523, "opt3", "desc3", false, true, "val3", null, null),
+                new ApiOptionItem(42, "opt4", "desc4", false, false, "val4", null, null)
+            )
+        )
+    );
+  }
+}
