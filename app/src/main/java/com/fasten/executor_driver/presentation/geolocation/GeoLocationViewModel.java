148a48216d85d22fa9e8ae46432e6151b2a0672e
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-03-16 17:07:31 +0300

    feat: модель вида местоположения с тестами.

---
+++ app/src/main/java/com/fasten/executor_driver/presentation/geolocation/GeoLocationViewModel.java
@@ -0,0 +1,10 @@
+package com.fasten.executor_driver.presentation.geolocation;
+
+import com.fasten.executor_driver.presentation.ViewModel;
+
+/**
+ * ViewModel окна карты.
+ */
+interface GeoLocationViewModel extends ViewModel<GeoLocationViewActions> {
+
+}
