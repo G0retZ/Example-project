5f0550428489dfe3ba996cf48d4c2cd3b48c75b0
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-03-16 15:40:45 +0300

    feat: убрал проверку разрешений на карте

--- app/src/main/java/com/fasten/executor_driver/view/MapFragment.java
+++ app/src/main/java/com/fasten/executor_driver/view/MapFragment.java
@@ -1,12 +1,8 @@
 package com.fasten.executor_driver.view;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.AlertDialog.Builder;
 import android.content.Context;
 import android.content.res.Resources;
-import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -26,21 +22,12 @@ import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.maps.android.data.geojson.GeoJsonFeature;
 import com.google.maps.android.data.geojson.GeoJsonLayer;
 import com.google.maps.android.data.geojson.GeoJsonPolygonStyle;
-import io.reactivex.disposables.Disposable;
 import javax.inject.Inject;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MapFragment extends SupportMapFragment implements OnMapReadyCallback,
     OnMyLocationButtonClickListener, MapViewActions {
-
-  private static final String[] PERMISSIONS = new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
-      Manifest.permission.ACCESS_COARSE_LOCATION};
-
-  @Nullable
-  private PermissionChecker permissionChecker;
-  @Nullable
-  private Disposable permissionDisposable;
 
   @Nullable
   private BaseActivity baseActivity;
@@ -105,15 +92,6 @@ public class MapFragment extends SupportMapFragment implements OnMapReadyCallbac
         viewState.apply(this);
       }
     });
-    resolvePermissions();
-  }
-
-  @Override
-  public void onDestroy() {
-    super.onDestroy();
-    if (permissionDisposable != null) {
-      permissionDisposable.dispose();
-    }
   }
 
   @Override
@@ -122,65 +100,24 @@ public class MapFragment extends SupportMapFragment implements OnMapReadyCallbac
     activity = null;
   }
 
-  private void resolvePermissions() {
-    permissionChecker = new PermissionChecker(1002);
-    permissionDisposable = permissionChecker.check(this, activity, PERMISSIONS)
-        .doFinally(() -> permissionChecker = null)
-        .subscribe(() -> initLocation(true),
-            throwable -> new Builder(activity)
-                .setTitle(R.string.warning)
-                .setMessage(getString(R.string.permissions_required))
-                .setPositiveButton(getString(android.R.string.ok),
-                    (dialog, which) -> activity.runOnUiThread(this::resolvePermissions))
-                .setNegativeButton(getString(android.R.string.cancel),
-                    ((dialog, which) -> initLocation(false)))
-                .create()
-                .show()
-        );
-  }
-
-  @Override
-  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-      @NonNull int[] grantResults) {
-    if (permissionChecker != null) {
-      permissionChecker.onResult(requestCode, permissions, grantResults);
-    }
-  }
-
-  @SuppressLint("MissingPermission")
-  private void initLocation(boolean locationTrackingAllowed) {
+  private void initLocation() {
     if (googleMap == null) {
       return;
     }
-    if (locationTrackingAllowed) {
-      googleMap.setMyLocationEnabled(true);
-      // Отправляем в Москву.
-      CameraPosition cameraPosition = new CameraPosition.Builder()
-          .target(new LatLng(55.737199, 37.628161)).zoom(15.2f)
-          .build();
-      googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-    } else {
-      // Отправляем в Москву.
-      CameraPosition cameraPosition = new CameraPosition.Builder()
-          .target(new LatLng(55.737199, 37.628161)).zoom(15.2f)
-          .build();
-      googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-    }
+//    googleMap.setMyLocationEnabled(true);
+    CameraPosition cameraPosition = new CameraPosition.Builder()
+        .target(new LatLng(55.737199, 37.628161)).zoom(15.2f)
+        .build();
+    googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
   }
 
   @Override
   public boolean onMyLocationButtonClick() {
-    //Временно
-    @SuppressWarnings("deprecation")
-    Location location = googleMap.getMyLocation();
-    if (location == null) {
-      return false;
-    }
     // Показываем, где он есть.
-    CameraPosition cameraPosition = new CameraPosition.Builder()
-        .target(new LatLng(location.getLatitude(), location.getLongitude())).zoom(15.2f)
-        .build();
-    googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+//    CameraPosition cameraPosition = new CameraPosition.Builder()
+//        .target(new LatLng(location.getLatitude(), location.getLongitude())).zoom(15.2f)
+//        .build();
+//    googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     return true;
   }
 
