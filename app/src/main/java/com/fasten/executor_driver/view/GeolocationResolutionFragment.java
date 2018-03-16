f9d2e3195e3a9dc963d810dfe7aa3c1167505bc2
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-03-16 15:14:39 +0300

    feat: Активити и фрагмент для решения проблем с доступом к геолокации.

---
+++ app/src/main/java/com/fasten/executor_driver/view/GeolocationResolutionFragment.java
@@ -0,0 +1,136 @@
+package com.fasten.executor_driver.view;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.fasten.executor_driver.R;
+import io.reactivex.disposables.Disposable;
+
+public class GeolocationResolutionFragment extends BaseFragment {
+
+  private static final String[] PERMISSIONS = new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+      Manifest.permission.ACCESS_COARSE_LOCATION};
+
+  @Nullable
+  private PermissionChecker permissionChecker;
+  @Nullable
+  private Disposable permissionDisposable;
+
+  private Activity activity;
+
+  @Override
+  public void onAttach(Context context) {
+    super.onAttach(context);
+    this.activity = (Activity) context;
+  }
+
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater,
+      @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.fragment_geo_resolution, container, false);
+    PendingIntent pendingIntent = activity.getIntent().getParcelableExtra("resolution");
+    if (pendingIntent != null) {
+      view.findViewById(R.id.resolve).setOnClickListener(v -> {
+        try {
+          // показать диалог через вызов startResolutionForResult(),
+          // и проверить результат в onActivityResult().
+          startIntentSenderForResult(pendingIntent.getIntentSender(), 1002, null, 0, 0, 0, null);
+        } catch (IntentSender.SendIntentException e) {
+          // Была ошибка с определением intent. Попробовать снова.
+          activity.finish();
+        }
+      });
+    } else {
+      view.findViewById(R.id.resolve).setOnClickListener(v -> resolvePermissions());
+    }
+    return view;
+  }
+
+  @Override
+  public boolean onBackPressed() {
+    return false;
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    if (permissionDisposable != null) {
+      permissionDisposable.dispose();
+    }
+  }
+
+  @Override
+  public void onDetach() {
+    super.onDetach();
+    activity = null;
+  }
+
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+      @NonNull int[] grantResults) {
+    switch (requestCode) {
+      case 1003:
+        if (permissionChecker != null) {
+          permissionChecker.onResult(requestCode, permissions, grantResults);
+        }
+        break;
+    }
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    switch (requestCode) {
+      case 1002:
+        activity.finish();
+        break;
+    }
+  }
+
+  private void resolvePermissions() {
+    permissionChecker = new PermissionChecker(1003);
+    boolean bool = true;
+    for (String permission : PERMISSIONS) {
+      bool = bool & shouldShowRequestPermissionRationale(permission);
+    }
+    boolean showRationaleBefore = bool;
+    permissionDisposable = permissionChecker.check(this, activity, PERMISSIONS)
+        .doFinally(() -> permissionChecker = null)
+        .subscribe(() -> activity.finish(),
+            throwable -> {
+              // user rejected the permission
+              boolean showRationale = true;
+              for (String permission : PERMISSIONS) {
+                showRationale = showRationale & shouldShowRequestPermissionRationale(permission);
+              }
+              if (!showRationale && !showRationaleBefore) {
+                // user also CHECKED "never ask again"
+                // you can either enable some fall back,
+                // disable features of your app
+                // or open another dialog explaining
+                // again the permission and directing to
+                // the app setting
+                startActivity(
+                    new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        .setData(
+                            Uri.fromParts("package", activity.getPackageName(), null)
+                        )
+                );
+              }
+            }
+        );
+  }
+}
