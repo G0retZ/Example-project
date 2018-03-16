819bc89b80efe2058e3c1dcff5fc076b8e956ac9
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-03-16 17:28:24 +0300

    style: поправил комментарий.

--- app/src/main/java/com/fasten/executor_driver/presentation/geolocation/GeoLocationViewModel.java
+++ app/src/main/java/com/fasten/executor_driver/presentation/geolocation/GeoLocationViewModel.java
@@ -3,7 +3,7 @@ package com.fasten.executor_driver.presentation.geolocation;
 import com.fasten.executor_driver.presentation.ViewModel;
 
 /**
- * ViewModel окна карты.
+ * ViewModel окна местоположения.
  */
 public interface GeoLocationViewModel extends ViewModel<GeoLocationViewActions> {
 
