d96081ce4a9c69d299453cf69e33f9b81d7e343b
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-03-28 16:53:54 +0300

    feat: задержечка для сплеша.

--- app/src/main/java/com/fasten/executor_driver/application/SplashScreenActivity.java
+++ app/src/main/java/com/fasten/executor_driver/application/SplashScreenActivity.java
@@ -1,8 +1,11 @@
 package com.fasten.executor_driver.application;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import com.fasten.executor_driver.R;
+import com.fasten.executor_driver.presentation.executorstate.ExecutorStateNavigate;
 
 public class SplashScreenActivity extends BaseActivity {
 
@@ -10,5 +13,29 @@ public class SplashScreenActivity extends BaseActivity {
   public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_splash);
+  }
+
+  @Override
+  public void navigate(@NonNull String destination) {
+    switch (destination) {
+      case ExecutorStateNavigate.AUTHORIZE:
+        navigateDelayed(destination);
+        break;
+      case ExecutorStateNavigate.MAP_SHIFT_CLOSED:
+        navigateDelayed(destination);
+        break;
+      case ExecutorStateNavigate.MAP_SHIFT_OPENED:
+        navigateDelayed(destination);
+        break;
+      case ExecutorStateNavigate.MAP_ONLINE:
+        navigateDelayed(destination);
+        break;
+      default:
+        super.navigate(destination);
+    }
+  }
+
+  private void navigateDelayed(@NonNull String destination) {
+    new Handler().postDelayed(() -> super.navigate(destination), 1500);
   }
 }
