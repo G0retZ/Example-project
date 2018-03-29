fb6d4e42efc8be22b42107aaf75d53d659feead4
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-03-29 17:45:34 +0300

    refactor: шарим статус между пользователями юзкейса.

--- app/src/main/java/com/fasten/executor_driver/interactor/ExecutorStateUseCaseImpl.java
+++ app/src/main/java/com/fasten/executor_driver/interactor/ExecutorStateUseCaseImpl.java
@@ -1,6 +1,7 @@
 package com.fasten.executor_driver.interactor;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import com.fasten.executor_driver.entity.ExecutorState;
 import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
@@ -14,6 +15,9 @@ public class ExecutorStateUseCaseImpl implements ExecutorStateUseCase {
   @NonNull
   private final DataReceiver<String> loginReceiver;
 
+  @Nullable
+  private Flowable<ExecutorState> executorStateFlowable;
+
   public ExecutorStateUseCaseImpl(@NonNull ExecutorStateGateway gateway,
       @NonNull SocketGateway socketGateway,
       @NonNull DataReceiver<String> loginReceiver) {
@@ -24,9 +28,14 @@ public class ExecutorStateUseCaseImpl implements ExecutorStateUseCase {
 
   @Override
   public Flowable<ExecutorState> getExecutorStates() {
-    return loginReceiver.get()
-        .toFlowable(BackpressureStrategy.BUFFER)
-        .startWith(socketGateway.openSocket().toFlowable())
-        .switchMap(gateway::getState);
+    if (executorStateFlowable == null) {
+      executorStateFlowable = loginReceiver.get()
+          .toFlowable(BackpressureStrategy.BUFFER)
+          .startWith(socketGateway.openSocket().toFlowable())
+          .switchMap(gateway::getState)
+          .replay(1)
+          .refCount();
+    }
+    return executorStateFlowable;
   }
 }
