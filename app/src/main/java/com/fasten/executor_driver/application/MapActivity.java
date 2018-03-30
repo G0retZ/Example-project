bcfd6750cc1d0015643ddf63e1236d5e0e40b3f6
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-03-30 15:58:06 +0300

    feat: открытие меню с карты.

--- app/src/main/java/com/fasten/executor_driver/application/MapActivity.java
+++ app/src/main/java/com/fasten/executor_driver/application/MapActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
+import android.view.MenuItem;
 import com.fasten.executor_driver.R;
 import com.fasten.executor_driver.presentation.executorstate.ExecutorStateNavigate;
 import com.fasten.executor_driver.presentation.onlinebutton.OnlineButtonNavigate;
@@ -22,6 +23,16 @@ public class MapActivity extends BaseActivity {
       actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_24dp);
       actionBar.setDisplayHomeAsUpEnabled(true);
     }
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case android.R.id.home:
+        startActivity(new Intent(this, MenuActivity.class));
+        return (true);
+    }
+    return (super.onOptionsItemSelected(item));
   }
 
   @Override
