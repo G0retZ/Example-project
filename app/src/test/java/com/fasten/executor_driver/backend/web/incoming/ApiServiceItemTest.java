e03e88ce18f589165c9c520ffcf878c76fb3f3ed
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-04-05 19:02:13 +0300

    feat: тест для модели ответа сервера по услугам.

--- app/src/test/java/com/fasten/executor_driver/backend/web/incoming/ApiServiceItemTest.java
+++ app/src/test/java/com/fasten/executor_driver/backend/web/incoming/ApiServiceItemTest.java
@@ -1,7 +1,35 @@
 package com.fasten.executor_driver.backend.web.incoming;
 
-// TODO: написать недостающие тесты.
-@SuppressWarnings("unused")
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class ApiServiceItemTest {
 
+  private ApiServiceItem apiServiceItem;
+
+  @Before
+  public void setUp() {
+    apiServiceItem = new ApiServiceItem(0, "name", 1500);
+  }
+
+  @Test
+  public void testConstructor() {
+    assertEquals(apiServiceItem.getId(), 0);
+    assertEquals(apiServiceItem.getName(), "name");
+    assertEquals(apiServiceItem.getPrice(), new Integer(1500));
+  }
+
+  @SuppressWarnings("SpellCheckingInspection")
+  @Test
+  public void testEquals() {
+    assertEquals(apiServiceItem, new ApiServiceItem(0, "name", 1500));
+    assertNotEquals(apiServiceItem, new ApiServiceItem(1, "name", 1500));
+    assertNotEquals(apiServiceItem, new ApiServiceItem(0, "nam", 1500));
+    assertNotEquals(apiServiceItem, new ApiServiceItem(0, null, 1500));
+    assertNotEquals(apiServiceItem, new ApiServiceItem(0, "name", 150));
+    assertNotEquals(apiServiceItem, new ApiServiceItem(0, "name", null));
+  }
 }
