67f5fec55a2da11b1ef42fcd00c7f8738ed03e53
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-04-18 16:07:08 +0300

    refactor: Убрал кастыль по получению номера телефона и подписке в юзкейсе + поправил тесты на это.

--- app/src/main/java/com/fasten/executor_driver/interactor/auth/PasswordUseCaseImpl.java
+++ app/src/main/java/com/fasten/executor_driver/interactor/auth/PasswordUseCaseImpl.java
@@ -1,6 +1,5 @@
 package com.fasten.executor_driver.interactor.auth;
 
-import android.annotation.SuppressLint;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import com.fasten.executor_driver.entity.LoginData;
@@ -16,7 +15,7 @@ public class PasswordUseCaseImpl implements PasswordUseCase {
   @NonNull
   private final Validator<String> passwordValidator;
   @NonNull
-  private LoginData loginData = new LoginData("", "");
+  private final DataReceiver<String> loginReceiver;
 
   @Inject
   public PasswordUseCaseImpl(@NonNull PasswordGateway gateway,
@@ -24,28 +23,20 @@ public class PasswordUseCaseImpl implements PasswordUseCase {
       @NonNull Validator<String> passwordValidator) {
     this.gateway = gateway;
     this.passwordValidator = passwordValidator;
-    loadLogin(loginReceiver);
-  }
-
-  @SuppressLint("CheckResult")
-  private void loadLogin(@NonNull DataReceiver<String> loginReceiver) {
-    loginReceiver.get()
-        .doAfterTerminate(() -> loadLogin(loginReceiver))
-        .subscribe(login -> loginData = loginData.setLogin(login),
-            throwable -> {
-            });
+    this.loginReceiver = loginReceiver;
   }
 
   @NonNull
   @Override
   public Completable authorize(@Nullable String password, @NonNull Completable afterValidation) {
-    return Completable.create(e -> {
-      passwordValidator.validate(password);
-      loginData = loginData.setPassword(password == null ? "" : password);
-      afterValidation.subscribe(
-          () -> gateway.authorize(loginData).subscribe(e::onComplete, e::onError),
-          throwable -> e.onComplete()
-      );
-    });
+    return loginReceiver.get()
+        .firstOrError()
+        .map(login -> {
+          passwordValidator.validate(password);
+          return new LoginData(login, password == null ? "" : password);
+        })
+        .flatMapCompletable(loginData ->
+            afterValidation.concatWith(gateway.authorize(loginData))
+        );
   }
 }
