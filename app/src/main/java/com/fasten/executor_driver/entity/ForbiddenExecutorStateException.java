a93e6d9330f2a3db612a4355ff3704e57440c0e2
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-04-27 16:53:23 +0300

    refactor: чуть больше логики для юзкейса с тестами.

---
+++ app/src/main/java/com/fasten/executor_driver/entity/ForbiddenExecutorStateException.java
@@ -0,0 +1,11 @@
+package com.fasten.executor_driver.entity;
+
+/**
+ * Исключение о запрешенном статусе исполнителя.
+ */
+public class ForbiddenExecutorStateException extends Exception {
+
+  public ForbiddenExecutorStateException() {
+    super();
+  }
+}
---
+++ app/src/main/java/com/fasten/executor_driver/interactor/ExecutorStateNotOnlineUseCaseImpl.java
@@ -0,0 +1,42 @@
+package com.fasten.executor_driver.interactor;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import com.fasten.executor_driver.entity.ExecutorState;
+import com.fasten.executor_driver.entity.ForbiddenExecutorStateException;
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+
+public class ExecutorStateNotOnlineUseCaseImpl implements
+    ExecutorStateNotOnlineUseCase {
+
+  @NonNull
+  private final ExecutorStateSwitchGateway executorStateSwitchGateway;
+  @NonNull
+  private final ExecutorStateUseCase executorStateUseCase;
+  @Nullable
+  private ExecutorState currentExecutorState;
+
+  ExecutorStateNotOnlineUseCaseImpl(
+      @NonNull ExecutorStateSwitchGateway executorStateSwitchGateway,
+      @NonNull ExecutorStateUseCase executorStateUseCase) {
+    this.executorStateSwitchGateway = executorStateSwitchGateway;
+    this.executorStateUseCase = executorStateUseCase;
+  }
+
+  @NonNull
+  @Override
+  public Flowable<ExecutorState> getExecutorStates() {
+    return executorStateUseCase.getExecutorStates(false)
+        .doOnNext(executorState -> currentExecutorState = executorState)
+        .doOnTerminate(() -> currentExecutorState = null);
+  }
+
+  @NonNull
+  @Override
+  public Completable setExecutorNotOnline() {
+    return currentExecutorState == null || currentExecutorState != ExecutorState.ONLINE
+        ? Completable.error(new ForbiddenExecutorStateException())
+        : executorStateSwitchGateway.sendNewExecutorState(ExecutorState.SHIFT_OPENED);
+  }
+}
