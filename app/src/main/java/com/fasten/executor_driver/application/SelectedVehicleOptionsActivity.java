854ed8924f9869c664a1b8fea17b30de0743b455
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-04-28 15:43:03 +0300

    feat: активити опций выбранного ТС.

--- app/src/main/java/com/fasten/executor_driver/application/SelectedVehicleOptionsActivity.java
+++ app/src/main/java/com/fasten/executor_driver/application/SelectedVehicleOptionsActivity.java
@@ -6,34 +6,33 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 import com.fasten.executor_driver.R;
-import com.fasten.executor_driver.presentation.onlineswitch.OnlineSwitchNavigate;
+import com.fasten.executor_driver.presentation.selectedvehicle.SelectedVehicleNavigate;
+import com.fasten.executor_driver.presentation.vehicleoptions.VehicleOptionsNavigate;
 
-public class OnlineActivity extends BaseActivity {
+public class SelectedVehicleOptionsActivity extends BaseActivity {
 
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_online);
+    setContentView(R.layout.activity_selected_vehicle_options);
     Toolbar toolbar = findViewById(R.id.appBar);
     if (toolbar != null) {
-      toolbar.setNavigationOnClickListener(
-          v -> startActivity(new Intent(this, MenuActivity.class))
-      );
+      toolbar.setNavigationOnClickListener(v -> onBackPressed());
     }
   }
 
   @Override
   public void navigate(@NonNull String destination) {
     switch (destination) {
-      case OnlineSwitchNavigate.SERVICES:
+      case VehicleOptionsNavigate.SERVICES:
         startActivity(new Intent(this, ServicesActivity.class));
+        finish();
         break;
-      case OnlineSwitchNavigate.VEHICLE_OPTIONS:
-        startActivity(new Intent(this, SelectedVehicleOptionsActivity.class));
+      case SelectedVehicleNavigate.VEHICLES:
+        startActivity(new Intent(this, ChooseVehicleActivity.class));
         break;
       default:
         super.navigate(destination);
-        break;
     }
   }
 }
