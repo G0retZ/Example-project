5a8422ddc4f85ec7a3e493fe71576ebfc7e2ce47
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-05-10 15:47:13 +0300

    fix: верный лайаут для активити.

--- app/src/main/java/com/fasten/executor_driver/application/OrderConfirmationActivity.java
+++ app/src/main/java/com/fasten/executor_driver/application/OrderConfirmationActivity.java
@@ -9,6 +9,6 @@ public class OrderConfirmationActivity extends BaseActivity {
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_offer);
+    setContentView(R.layout.activity_order_confirmation);
   }
 }
