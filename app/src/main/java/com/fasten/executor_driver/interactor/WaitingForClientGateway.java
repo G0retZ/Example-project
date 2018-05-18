7caff3105962678779225005bd0edad3378da985
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-05-18 12:24:17 +0300

    feat: Юзкейс ожидания клиента с тестами.

---
+++ app/src/main/java/com/fasten/executor_driver/interactor/WaitingForClientGateway.java
@@ -0,0 +1,26 @@
+package com.fasten.executor_driver.interactor;
+
+import android.support.annotation.NonNull;
+import io.reactivex.Completable;
+
+/**
+ * Гейтвей работы с заказами при движении к клиенту.
+ */
+interface WaitingForClientGateway {
+
+  /**
+   * Передает запрос звонка клиенту.
+   *
+   * @return {@link Completable} результат - успех либо ошибка на сервере.
+   */
+  @NonNull
+  Completable callToClient();
+
+  /**
+   * Передает сообщение о прибытии к клиенту.
+   *
+   * @return {@link Completable} результат - успех либо ошибка на сервере.
+   */
+  @NonNull
+  Completable startTheOrder();
+}
---
+++ app/src/main/java/com/fasten/executor_driver/interactor/WaitingForClientUseCaseImpl.java
@@ -0,0 +1,40 @@
+package com.fasten.executor_driver.interactor;
+
+import android.support.annotation.NonNull;
+import com.fasten.executor_driver.entity.ExecutorState;
+import com.fasten.executor_driver.entity.Order;
+import io.reactivex.Completable;
+import io.reactivex.Flowable;
+import javax.inject.Inject;
+
+class WaitingForClientUseCaseImpl implements WaitingForClientUseCase {
+
+  @NonNull
+  private final OrderGateway orderGateway;
+  @NonNull
+  private final WaitingForClientGateway waitingForClientGateway;
+
+  @Inject
+  WaitingForClientUseCaseImpl(@NonNull OrderGateway orderGateway,
+      @NonNull WaitingForClientGateway waitingForClientGateway) {
+    this.orderGateway = orderGateway;
+    this.waitingForClientGateway = waitingForClientGateway;
+  }
+
+  @Override
+  public Flowable<Order> getOrders() {
+    return orderGateway.getOrders(ExecutorState.WAITING_FOR_CLIENT);
+  }
+
+  @NonNull
+  @Override
+  public Completable callToClient() {
+    return waitingForClientGateway.callToClient();
+  }
+
+  @NonNull
+  @Override
+  public Completable startTheOrder() {
+    return waitingForClientGateway.startTheOrder();
+  }
+}
