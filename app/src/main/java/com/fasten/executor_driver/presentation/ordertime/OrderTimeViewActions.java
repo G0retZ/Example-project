4cfd5eaabbd5ed3d4fcc3497c9ee068e66331e21
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-05-24 18:05:10 +0300

    feat: состояния вида времени выполнения заказа с тестами.

---
+++ app/src/main/java/com/fasten/executor_driver/presentation/ordertime/OrderTimeViewActions.java
@@ -0,0 +1,21 @@
+package com.fasten.executor_driver.presentation.ordertime;
+
+/**
+ * Действия для смены состояния вида текущего времени заказа.
+ */
+interface OrderTimeViewActions {
+
+  /**
+   * Задать общее время заказа.
+   *
+   * @param currentSeconds - текущее время заказа в секундах.
+   */
+  void setOrderTimeText(long currentSeconds);
+
+  /**
+   * Показать сообщение об ошибке сети.
+   *
+   * @param show - показать или нет?
+   */
+  void showOrderTimeNetworkErrorMessage(boolean show);
+}
