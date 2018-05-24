c1c6120dacfebb784aa8821aad033a455e34a40a
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-05-24 13:48:49 +0300

    feat: гейтвей суммы стоимости заказа сверх пакета с тестами.

--- app/src/main/java/com/fasten/executor_driver/interactor/OrderExcessCostGateway.java
+++ app/src/main/java/com/fasten/executor_driver/interactor/OrderExcessCostGateway.java
@@ -1,16 +1,18 @@
 package com.fasten.executor_driver.interactor;
 
+import android.support.annotation.NonNull;
 import io.reactivex.Flowable;
 
 /**
  * Гейтвей цены выполнения заказа.
  */
-interface OrderExcessCostGateway {
+public interface OrderExcessCostGateway {
 
   /**
    * Запрашивает изменения цены выполняемого заказа.
    *
    * @return {@link Flowable<Integer>} результат запроса.
    */
+  @NonNull
   Flowable<Integer> getOrderExcessCost();
 }
---
+++ app/src/test/java/com/fasten/executor_driver/interactor/OrderExcessCostGatewayTest.java
@@ -0,0 +1,282 @@
+package com.fasten.executor_driver.interactor;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import com.fasten.executor_driver.backend.web.NoNetworkException;
+import com.fasten.executor_driver.backend.websocket.ConnectionClosedException;
+import com.fasten.executor_driver.gateway.DataMappingException;
+import com.fasten.executor_driver.gateway.Mapper;
+import com.fasten.executor_driver.gateway.OrderExcessCostGatewayImpl;
+import io.reactivex.Observable;
+import io.reactivex.plugins.RxJavaPlugins;
+import io.reactivex.schedulers.Schedulers;
+import io.reactivex.subscribers.TestSubscriber;
+import java.util.ArrayList;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import ua.naiksoftware.stomp.client.StompClient;
+import ua.naiksoftware.stomp.client.StompMessage;
+
+@RunWith(MockitoJUnitRunner.class)
+public class OrderExcessCostGatewayTest {
+
+  private OrderExcessCostGateway executorStateGateway;
+
+  @Mock
+  private StompClient stompClient;
+  @Mock
+  private Mapper<StompMessage, Integer> mapper;
+
+  @Before
+  public void setUp() {
+    RxJavaPlugins.setIoSchedulerHandler(scheduler -> Schedulers.trampoline());
+    RxJavaPlugins.setSingleSchedulerHandler(scheduler -> Schedulers.trampoline());
+    executorStateGateway = new OrderExcessCostGatewayImpl(stompClient, mapper);
+    when(stompClient.topic(anyString())).thenReturn(Observable.never());
+  }
+
+  /* Проверяем работу с клиентом STOMP */
+
+  /**
+   * Должен запросить у клиента STOMP статусы, если он соединен и не соединяется.
+   */
+  @Test
+  public void askStompClientForStatus() {
+    // Дано:
+    InOrder inOrder = Mockito.inOrder(stompClient);
+    when(stompClient.isConnected()).thenReturn(true);
+
+    // Действие:
+    executorStateGateway.getOrderExcessCost().test();
+
+    // Результат:
+    inOrder.verify(stompClient).isConnected();
+    inOrder.verify(stompClient).topic("/mobile/order/ecost");
+    verifyNoMoreInteractions(stompClient);
+  }
+
+  /**
+   * Не должен просить у клиента STOMP соединение, если он не соединен и не соединяется.
+   */
+  @Test
+  public void doNotAskStompClientToConnectOrForStatus() {
+    // Дано:
+    InOrder inOrder = Mockito.inOrder(stompClient);
+
+    // Действие:
+    executorStateGateway.getOrderExcessCost().test();
+
+    // Результат:
+    inOrder.verify(stompClient).isConnected();
+    inOrder.verify(stompClient).isConnecting();
+    verifyNoMoreInteractions(stompClient);
+  }
+
+  /**
+   * Должен запросить у клиента STOMP статусы, если он не соединен и соединяется.
+   */
+  @Test
+  public void askStompClientForStatusIfConnecting() {
+    // Дано:
+    InOrder inOrder = Mockito.inOrder(stompClient);
+    when(stompClient.isConnecting()).thenReturn(true);
+
+    // Действие:
+    executorStateGateway.getOrderExcessCost().test();
+
+    // Результат:
+    inOrder.verify(stompClient).isConnected();
+    inOrder.verify(stompClient).isConnecting();
+    inOrder.verify(stompClient).topic("/mobile/order/ecost");
+    verifyNoMoreInteractions(stompClient);
+  }
+
+  /* Проверяем работу с маппером */
+
+  /**
+   * Должен запросить маппинг, если он соединен и не соединяется.
+   *
+   * @throws Exception error
+   */
+  @Test
+  public void askForMappingForStatusHeaderIfConnected() throws Exception {
+    // Дано:
+    when(stompClient.isConnected()).thenReturn(true);
+    when(stompClient.topic(anyString())).thenReturn(Observable.just(
+        new StompMessage("MESSAGE", new ArrayList<>(), "\n")
+    ));
+
+    // Действие:
+    executorStateGateway.getOrderExcessCost().test();
+
+    // Результат:
+    verify(mapper, only()).map(any());
+  }
+
+  /**
+   * Должен запросить маппинг после соединения, если он не соединен и соединяется.
+   *
+   * @throws Exception error
+   */
+  @Test
+  public void askForMappingForStatusHeaderIfConnectingAfterConnected() throws Exception {
+    // Дано:
+    when(stompClient.isConnecting()).thenReturn(true);
+    when(stompClient.topic(anyString())).thenReturn(Observable.just(
+        new StompMessage("MESSAGE", new ArrayList<>(), "\n")
+    ));
+
+    // Действие:
+    executorStateGateway.getOrderExcessCost().test();
+
+    // Результат:
+    verify(mapper, only()).map(any());
+  }
+
+  /* Проверяем правильность потоков (добавить) */
+
+  /* Проверяем результаты обработки сообщений от сервера по статусам */
+
+  /**
+   * Должен ответить ошибкой маппинга, если он соединен и не соединяется.
+   *
+   * @throws Exception error
+   */
+  @Test
+  public void answerDataMappingErrorForStatusHeaderIfConnected() throws Exception {
+    // Дано:
+    doThrow(new DataMappingException()).when(mapper).map(any());
+    when(stompClient.isConnected()).thenReturn(true);
+    when(stompClient.topic(anyString())).thenReturn(Observable.just(
+        new StompMessage("MESSAGE", new ArrayList<>(), "\n")
+    ));
+
+    // Действие:
+    TestSubscriber<Integer> testSubscriber = executorStateGateway.getOrderExcessCost().test();
+
+    // Результат:
+    testSubscriber.assertError(DataMappingException.class);
+  }
+
+  /**
+   * Должен вернуть число, если он соединен и не соединяется.
+   *
+   * @throws Exception error
+   */
+  @Test
+  public void answerShiftOpenedForStatusHeaderIfConnected() throws Exception {
+    // Дано:
+    when(mapper.map(any())).thenReturn(12345);
+    when(stompClient.isConnected()).thenReturn(true);
+    when(stompClient.topic(anyString())).thenReturn(Observable.just(
+        new StompMessage("MESSAGE", new ArrayList<>(), "\n")
+    ));
+
+    // Действие:
+    TestSubscriber<Integer> testSubscriber = executorStateGateway.getOrderExcessCost().test();
+
+    // Результат:
+    testSubscriber.assertValue(12345);
+  }
+
+  /**
+   * Должен ответить ошибкой, если он соединен и не соединяется.
+   */
+  @Test
+  public void answerErrorIfConnected() {
+    // Дано:
+    when(stompClient.isConnected()).thenReturn(true);
+    when(stompClient.topic(anyString())).thenReturn(Observable.error(new NoNetworkException()));
+
+    // Действие:
+    TestSubscriber<Integer> testSubscriber = executorStateGateway.getOrderExcessCost().test();
+
+    // Результат:
+    testSubscriber.assertError(NoNetworkException.class);
+  }
+
+  /**
+   * Должен ответить ошибкой, если он не соединен и не соединяется.
+   */
+  @Test
+  public void answerConnectionErrorIfNotConnectingAfterConnected() {
+    // Действие:
+    TestSubscriber<Integer> testSubscriber = executorStateGateway.getOrderExcessCost().test();
+
+    // Результат:
+    testSubscriber.assertError(ConnectionClosedException.class);
+  }
+
+  /**
+   * Должен ответить ошибкой маппинга, если он не соединен и соединяется.
+   *
+   * @throws Exception error
+   */
+  @Test
+  public void answerDataMappingErrorForStatusHeaderIfConnectingAfterConnected() throws Exception {
+    // Дано:
+    doThrow(new DataMappingException()).when(mapper).map(any());
+    when(stompClient.isConnecting()).thenReturn(true);
+    when(stompClient.topic(anyString())).thenReturn(Observable.just(
+        new StompMessage("MESSAGE", new ArrayList<>(), "\n")
+    ));
+
+    // Действие:
+    TestSubscriber<Integer> testSubscriber = executorStateGateway.getOrderExcessCost().test();
+
+    // Результат:
+    testSubscriber.assertError(DataMappingException.class);
+    testSubscriber.assertNoValues();
+  }
+
+  /**
+   * Должен вернуть число, если он не соединен и соединяется.
+   *
+   * @throws Exception error
+   */
+  @Test
+  public void answerShiftOpenedForStatusHeaderIfConnectingAfterConnected() throws Exception {
+    // Дано:
+    when(mapper.map(any())).thenReturn(54321);
+    when(stompClient.isConnecting()).thenReturn(true);
+    when(stompClient.topic(anyString())).thenReturn(Observable.just(
+        new StompMessage("MESSAGE", new ArrayList<>(), "\n")
+    ));
+
+    // Действие:
+    TestSubscriber<Integer> testSubscriber = executorStateGateway.getOrderExcessCost().test();
+
+    // Результат:
+    testSubscriber.assertValue(54321);
+    testSubscriber.assertNoErrors();
+  }
+
+  /**
+   * Должен ответить ошибкой, если он не соединен и соединяется.
+   */
+  @Test
+  public void answerErrorIfConnecting() {
+    // Дано:
+    when(stompClient.isConnecting()).thenReturn(true);
+    when(stompClient.topic(anyString()))
+        .thenReturn(Observable.error(new ConnectionClosedException()));
+
+    // Действие:
+    TestSubscriber<Integer> testSubscriber = executorStateGateway.getOrderExcessCost().test();
+
+    // Результат:
+    testSubscriber.assertError(ConnectionClosedException.class);
+    testSubscriber.assertNoValues();
+  }
+}
