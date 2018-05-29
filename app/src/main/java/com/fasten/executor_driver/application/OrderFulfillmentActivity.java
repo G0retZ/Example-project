1135e84644764c527730b78f5674986dd0dbe5a8
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-05-29 11:53:32 +0300

    feat: меню действий экрана выполнения заказа.

--- app/src/main/java/com/fasten/executor_driver/application/OrderFulfillmentActivity.java
+++ app/src/main/java/com/fasten/executor_driver/application/OrderFulfillmentActivity.java
@@ -2,9 +2,12 @@ package com.fasten.executor_driver.application;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 import com.fasten.executor_driver.R;
+import com.fasten.executor_driver.presentation.oderfulfillmentmenu.OrderFulfillmentMenuNavigate;
+import com.fasten.executor_driver.view.OrderFulfillmentActionsDialogFragment;
 
 public class OrderFulfillmentActivity extends BaseActivity {
 
@@ -16,8 +19,23 @@ public class OrderFulfillmentActivity extends BaseActivity {
     if (toolbar != null) {
       toolbar.setNavigationOnClickListener(v -> onBackPressed());
       toolbar.findViewById(R.id.orderActions).setOnClickListener(
-          v -> startActivity(new Intent(this, OrderRouteActivity.class))
+          v -> {
+            if (getSupportFragmentManager().findFragmentByTag("menu") == null) {
+              new OrderFulfillmentActionsDialogFragment().show(getSupportFragmentManager(), "menu");
+            }
+          }
       );
+    }
+  }
+
+  @Override
+  public void navigate(@NonNull String destination) {
+    switch (destination) {
+      case OrderFulfillmentMenuNavigate.ORDER_ROUTE:
+        startActivity(new Intent(this, OrderRouteActivity.class));
+        break;
+      default:
+        super.navigate(destination);
     }
   }
 }
