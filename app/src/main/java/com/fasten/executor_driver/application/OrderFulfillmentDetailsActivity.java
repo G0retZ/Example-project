ccadd4efd0e610d9b485338670d06669a94a740f
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-05-31 10:21:32 +0300

    feat: навигация назад по кнопке "вверх".

--- app/src/main/java/com/fasten/executor_driver/application/OrderFulfillmentDetailsActivity.java
+++ app/src/main/java/com/fasten/executor_driver/application/OrderFulfillmentDetailsActivity.java
@@ -2,6 +2,7 @@ package com.fasten.executor_driver.application;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
 import com.fasten.executor_driver.R;
 
 public class OrderFulfillmentDetailsActivity extends BaseActivity {
@@ -10,5 +11,9 @@ public class OrderFulfillmentDetailsActivity extends BaseActivity {
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_order_fulfillment_details);
+    Toolbar toolbar = findViewById(R.id.appBar);
+    if (toolbar != null) {
+      toolbar.setNavigationOnClickListener(v -> onBackPressed());
+    }
   }
 }
