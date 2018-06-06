3daaa1ad9911576e3583168ee48851a0655279bc
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-06-06 12:09:27 +0300

    feat: модель-маппер АПИ причины отказа из бизнес сущности для отправки в сокет.

---
+++ app/src/test/java/com/fasten/executor_driver/backend/websocket/outgoing/ApiCancelOrderReasonTest.java
@@ -0,0 +1,31 @@
+package com.fasten.executor_driver.backend.websocket.outgoing;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import com.fasten.executor_driver.entity.CancelOrderReason;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ApiCancelOrderReasonTest {
+
+  @Mock
+  private CancelOrderReason cancelOrderReason;
+
+  @Test
+  public void testConstructor() {
+    // Дано:
+    when(cancelOrderReason.getId()).thenReturn(7);
+    when(cancelOrderReason.getName()).thenReturn("seven");
+
+    // Действие:
+    ApiCancelOrderReason apiCancelOrderReason = new ApiCancelOrderReason(cancelOrderReason);
+
+    // Результат:
+    assertEquals(apiCancelOrderReason.getId(), 7);
+    assertEquals(apiCancelOrderReason.getDescription(), "seven");
+  }
+}
