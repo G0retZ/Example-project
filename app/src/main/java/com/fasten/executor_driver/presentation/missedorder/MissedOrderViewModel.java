fe64dceec20250acf283f1e48ac50fb1783a3c9e
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-06-09 18:43:13 +0300

    feat: модель и действия над видом сообщений об упущенных заказах с тестами

---
+++ app/src/main/java/com/fasten/executor_driver/presentation/missedorder/MissedOrderViewModel.java
@@ -0,0 +1,14 @@
+package com.fasten.executor_driver.presentation.missedorder;
+
+import com.fasten.executor_driver.presentation.ViewModel;
+
+/**
+ * ViewModel сообщений об упущенных заказах.
+ */
+interface MissedOrderViewModel extends ViewModel<MissedOrderViewActions> {
+
+  /**
+   * Запрашивает подписку на сообщения об упущенных заказах.
+   */
+  void initializeMissedOrderMessages();
+}
