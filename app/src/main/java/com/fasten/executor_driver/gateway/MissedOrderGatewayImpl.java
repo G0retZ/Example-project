4f11de3c75b00ae0bc984fbaf4ac645e56bea9ba
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-06-09 17:58:09 +0300

    feat: гейтвей сообщений об упущенных заказах

---
+++ app/src/main/java/com/fasten/executor_driver/gateway/MissedOrderGatewayImpl.java
@@ -0,0 +1,34 @@
+package com.fasten.executor_driver.gateway;
+
+import android.support.annotation.NonNull;
+import com.fasten.executor_driver.BuildConfig;
+import com.fasten.executor_driver.backend.websocket.ConnectionClosedException;
+import com.fasten.executor_driver.interactor.MissedOrderGateway;
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.schedulers.Schedulers;
+import ua.naiksoftware.stomp.client.StompClient;
+
+public class MissedOrderGatewayImpl implements MissedOrderGateway {
+
+  @NonNull
+  private final StompClient stompClient;
+
+  public MissedOrderGatewayImpl(@NonNull StompClient stompClient) {
+    this.stompClient = stompClient;
+  }
+
+  @NonNull
+  @Override
+  public Flowable<String> loadMissedOrdersMessages(@NonNull String channelId) {
+    if (stompClient.isConnected() || stompClient.isConnecting()) {
+      return stompClient.topic(String.format(BuildConfig.STATUS_DESTINATION, channelId))
+          .toFlowable(BackpressureStrategy.BUFFER)
+          .subscribeOn(Schedulers.io())
+          .observeOn(Schedulers.single())
+          .filter(stompMessage -> stompMessage.findHeader("MissedOrder") != null)
+          .map(stompMessage -> stompMessage.getPayload().trim());
+    }
+    return Flowable.error(new ConnectionClosedException());
+  }
+}
