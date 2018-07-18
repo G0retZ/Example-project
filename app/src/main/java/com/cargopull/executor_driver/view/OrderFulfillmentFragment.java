35e32d53bc6d91a96d91fa519b36180f7cd07706
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-07-18 18:04:55 +0300

    feat: Кнопки "сообщить о прибытии" и "завершить услугу" при выполнении заказа теперь требуют долгого нажатия.

--- app/src/main/java/com/cargopull/executor_driver/view/OrderFulfillmentFragment.java
+++ app/src/main/java/com/cargopull/executor_driver/view/OrderFulfillmentFragment.java
@@ -1,5 +1,9 @@
 package com.cargopull.executor_driver.view;
 
+import android.animation.Animator;
+import android.animation.Animator.AnimatorListener;
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog.Builder;
 import android.content.Context;
 import android.content.Intent;
@@ -8,9 +12,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import com.cargopull.executor_driver.R;
 import com.cargopull.executor_driver.di.AppComponent;
@@ -42,9 +50,19 @@ public class OrderFulfillmentFragment extends BaseFragment implements OrderCostV
   private TextView commentTitleText;
   private TextView commentText;
   private Button getDirectionsAction;
-  private Button closeRoutePointAction;
-  private Button completeTheOrderAction;
+  private ProgressBar closeRoutePointAction;
+  private TextView closeRoutePointText;
+  private ProgressBar completeTheOrderAction;
+  private TextView completeTheOrderText;
   private Context context;
+  @Nullable
+  private ObjectAnimator closeRoutePointDelayAnimator;
+  @Nullable
+  private ObjectAnimator closeRoutePointResetAnimator;
+  @Nullable
+  private ObjectAnimator completeTheOrderDelayAnimator;
+  @Nullable
+  private ObjectAnimator completeTheOrderResetAnimator;
 
   @Inject
   public void setOrderCostViewModel(OrderCostViewModel orderCostViewModel) {
@@ -67,6 +85,7 @@ public class OrderFulfillmentFragment extends BaseFragment implements OrderCostV
     this.context = context;
   }
 
+  @SuppressLint("ClickableViewAccessibility")
   @Nullable
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater,
@@ -81,9 +100,108 @@ public class OrderFulfillmentFragment extends BaseFragment implements OrderCostV
     commentText = view.findViewById(R.id.commentText);
     getDirectionsAction = view.findViewById(R.id.openNavigator);
     closeRoutePointAction = view.findViewById(R.id.closeRoutePoint);
-    closeRoutePointAction.setOnClickListener(v -> nextRoutePointViewModel.closeRoutePoint());
+    closeRoutePointText = view.findViewById(R.id.closeRoutePointText);
     completeTheOrderAction = view.findViewById(R.id.completeTheOrder);
-    completeTheOrderAction.setOnClickListener(v -> nextRoutePointViewModel.completeTheOrder());
+    completeTheOrderText = view.findViewById(R.id.completeTheOrderText);
+
+    closeRoutePointDelayAnimator = ObjectAnimator.ofInt(closeRoutePointAction, "progress", 0, 100);
+    closeRoutePointDelayAnimator.setDuration(1500);
+    closeRoutePointDelayAnimator.setInterpolator(new DecelerateInterpolator());
+    closeRoutePointDelayAnimator.addListener(new AnimatorListener() {
+      private boolean canceled;
+
+      @Override
+      public void onAnimationStart(Animator animation) {
+        canceled = false;
+      }
+
+      @Override
+      public void onAnimationEnd(Animator animation) {
+        if (!canceled) {
+          nextRoutePointViewModel.closeRoutePoint();
+        }
+      }
+
+      @Override
+      public void onAnimationCancel(Animator animation) {
+        canceled = true;
+      }
+
+      @Override
+      public void onAnimationRepeat(Animator animation) {
+
+      }
+    });
+
+    closeRoutePointAction.setOnTouchListener((v, event) -> {
+      int i = event.getAction();
+      if (i == MotionEvent.ACTION_DOWN) {
+        closeRoutePointDelayAnimator.start();
+        if (closeRoutePointResetAnimator != null) {
+          closeRoutePointResetAnimator.cancel();
+        }
+        return true;
+      } else if (i == MotionEvent.ACTION_UP) {
+        closeRoutePointDelayAnimator.cancel();
+        closeRoutePointResetAnimator = ObjectAnimator
+            .ofInt(closeRoutePointAction, "progress", closeRoutePointAction.getProgress(), 0);
+        closeRoutePointResetAnimator.setDuration(150);
+        closeRoutePointResetAnimator.setInterpolator(new LinearInterpolator());
+        closeRoutePointResetAnimator.start();
+        return true;
+      }
+      return false;
+    });
+
+    completeTheOrderDelayAnimator = ObjectAnimator
+        .ofInt(completeTheOrderAction, "progress", 0, 100);
+    completeTheOrderDelayAnimator.setDuration(1500);
+    completeTheOrderDelayAnimator.setInterpolator(new DecelerateInterpolator());
+    completeTheOrderDelayAnimator.addListener(new AnimatorListener() {
+      private boolean canceled;
+
+      @Override
+      public void onAnimationStart(Animator animation) {
+        canceled = false;
+      }
+
+      @Override
+      public void onAnimationEnd(Animator animation) {
+        if (!canceled) {
+          nextRoutePointViewModel.completeTheOrder();
+        }
+      }
+
+      @Override
+      public void onAnimationCancel(Animator animation) {
+        canceled = true;
+      }
+
+      @Override
+      public void onAnimationRepeat(Animator animation) {
+
+      }
+    });
+
+    completeTheOrderAction.setOnTouchListener((v, event) -> {
+      int i = event.getAction();
+      if (i == MotionEvent.ACTION_DOWN) {
+        completeTheOrderDelayAnimator.start();
+        if (completeTheOrderResetAnimator != null) {
+          completeTheOrderResetAnimator.cancel();
+        }
+        return true;
+      } else if (i == MotionEvent.ACTION_UP) {
+        completeTheOrderDelayAnimator.cancel();
+        completeTheOrderResetAnimator = ObjectAnimator
+            .ofInt(completeTheOrderAction, "progress", completeTheOrderAction.getProgress(), 0);
+        completeTheOrderResetAnimator.setDuration(150);
+        completeTheOrderResetAnimator.setInterpolator(new LinearInterpolator());
+        completeTheOrderResetAnimator.start();
+        return true;
+      }
+      return false;
+    });
     return view;
   }
 
@@ -130,6 +248,18 @@ public class OrderFulfillmentFragment extends BaseFragment implements OrderCostV
 
   @Override
   public void onDetach() {
+    if (closeRoutePointResetAnimator != null) {
+      closeRoutePointResetAnimator.cancel();
+    }
+    if (closeRoutePointDelayAnimator != null) {
+      closeRoutePointDelayAnimator.cancel();
+    }
+    if (completeTheOrderResetAnimator != null) {
+      completeTheOrderResetAnimator.cancel();
+    }
+    if (completeTheOrderDelayAnimator != null) {
+      completeTheOrderDelayAnimator.cancel();
+    }
     super.onDetach();
     context = null;
   }
@@ -182,11 +312,13 @@ public class OrderFulfillmentFragment extends BaseFragment implements OrderCostV
   @Override
   public void showCloseNextRoutePointAction(boolean show) {
     closeRoutePointAction.setVisibility(show ? View.VISIBLE : View.GONE);
+    closeRoutePointText.setVisibility(show ? View.VISIBLE : View.GONE);
   }
 
   @Override
   public void showCompleteOrderAction(boolean show) {
     completeTheOrderAction.setVisibility(show ? View.VISIBLE : View.GONE);
+    completeTheOrderText.setVisibility(show ? View.VISIBLE : View.GONE);
   }
 
   @Override
