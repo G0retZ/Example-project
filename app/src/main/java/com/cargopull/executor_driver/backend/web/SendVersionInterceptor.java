de5d62d82c7213a37c940314a9a39fd5d5fb7ee9
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-07-24 16:06:21 +0300

    feat: Подставляем версию приложения в хедеры HTTP зыпросов.

---
+++ app/src/main/java/com/cargopull/executor_driver/backend/web/SendVersionInterceptor.java
@@ -0,0 +1,28 @@
+package com.cargopull.executor_driver.backend.web;
+
+import com.cargopull.executor_driver.BuildConfig;
+import java.io.IOException;
+import javax.inject.Inject;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Перехватчик для подстановки версии приложения в заголовки.
+ */
+public class SendVersionInterceptor implements Interceptor {
+
+  private final static String HEADER_NAME = "X-ea-app-version";
+
+  @Inject
+  public SendVersionInterceptor() {
+  }
+
+  @Override
+  public Response intercept(Chain chain) throws IOException {
+    Request.Builder builder = chain.request().newBuilder();
+    System.out.println("OkHttp Adding Header: " + BuildConfig.VERSION_NAME);
+    builder.addHeader(HEADER_NAME, BuildConfig.VERSION_NAME);
+    return chain.proceed(builder.build());
+  }
+}
