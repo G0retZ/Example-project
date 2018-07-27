ce0f0d2817b85c3340ca297a6df05d63067536f6
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-07-27 17:53:30 +0300

    feat: юзкейс сообщений о новой версии.

---
+++ app/src/main/java/com/cargopull/executor_driver/interactor/UpdateMessageUseCase.java
@@ -0,0 +1,18 @@
+package com.cargopull.executor_driver.interactor;
+
+import android.support.annotation.NonNull;
+import io.reactivex.Flowable;
+
+/**
+ * Юзкейс получения сообщений о новой версии.
+ */
+interface UpdateMessageUseCase {
+
+  /**
+   * Запрашивает сообщения о доступности новой версии.
+   *
+   * @return {@link Flowable<String>} результат запроса.
+   */
+  @NonNull
+  Flowable<String> getUpdateMessages();
+}
