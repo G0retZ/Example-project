8bc0ae45f95bacb0be9427b47fc1a94e53016b74
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-08-17 18:53:35 +0300

    feat: тулбар для экрана предзаказа.

--- app/src/main/java/com/cargopull/executor_driver/application/DriverPreOrderConfirmationActivity.java
+++ app/src/main/java/com/cargopull/executor_driver/application/DriverPreOrderConfirmationActivity.java
@@ -3,6 +3,7 @@ package com.cargopull.executor_driver.application;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
 import com.cargopull.executor_driver.R;
 import com.cargopull.executor_driver.presentation.orderconfirmation.OrderConfirmationNavigate;
 
@@ -12,6 +13,10 @@ public class DriverPreOrderConfirmationActivity extends BaseActivity {
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_driver_pre_order_confirmation);
+    Toolbar toolbar = findViewById(R.id.appBar);
+    if (toolbar != null) {
+      toolbar.setNavigationOnClickListener(v -> onBackPressed());
+    }
   }
 
   @Override
@@ -22,7 +27,6 @@ public class DriverPreOrderConfirmationActivity extends BaseActivity {
         break;
       default:
         super.navigate(destination);
-        break;
     }
   }
 }
