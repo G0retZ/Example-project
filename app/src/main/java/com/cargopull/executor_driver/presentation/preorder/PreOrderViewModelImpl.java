a0bef848ae87074761002ff812644194ee404cef
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-08-29 20:53:50 +0300

    feat: Модель вида оповещения о предзаказе с состояниями вида и тестами.

---
+++ app/src/main/java/com/cargopull/executor_driver/presentation/preorder/PreOrderViewModelImpl.java
@@ -0,0 +1,77 @@
+package com.cargopull.executor_driver.presentation.preorder;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.support.annotation.NonNull;
+import com.cargopull.executor_driver.gateway.DataMappingException;
+import com.cargopull.executor_driver.interactor.OrderUseCase;
+import com.cargopull.executor_driver.presentation.CommonNavigate;
+import com.cargopull.executor_driver.presentation.SingleLiveEvent;
+import com.cargopull.executor_driver.presentation.ViewState;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.internal.disposables.EmptyDisposable;
+import javax.inject.Inject;
+
+public class PreOrderViewModelImpl extends ViewModel implements
+    PreOrderViewModel {
+
+  @NonNull
+  private final OrderUseCase orderUseCase;
+  @NonNull
+  private final MutableLiveData<ViewState<PreOrderViewActions>> viewStateLiveData;
+  @NonNull
+  private final SingleLiveEvent<String> navigateLiveData;
+  @NonNull
+  private Disposable disposable = EmptyDisposable.INSTANCE;
+
+  @Inject
+  public PreOrderViewModelImpl(@NonNull OrderUseCase orderUseCase) {
+    this.orderUseCase = orderUseCase;
+    viewStateLiveData = new MutableLiveData<>();
+    navigateLiveData = new SingleLiveEvent<>();
+    loadOrders();
+  }
+
+  @NonNull
+  @Override
+  public LiveData<ViewState<PreOrderViewActions>> getViewStateLiveData() {
+    return viewStateLiveData;
+  }
+
+  @NonNull
+  @Override
+  public LiveData<String> getNavigationLiveData() {
+    return navigateLiveData;
+  }
+
+
+  private void loadOrders() {
+    if (disposable.isDisposed()) {
+      viewStateLiveData.postValue(new PreOrderViewStateUnAvailable());
+      disposable = orderUseCase.getOrders()
+          .observeOn(AndroidSchedulers.mainThread())
+          .doOnComplete(() -> viewStateLiveData.postValue(new PreOrderViewStateUnAvailable()))
+          .repeat()
+          .subscribe(order -> viewStateLiveData.postValue(new PreOrderViewStateAvailable()),
+              throwable -> {
+                if (throwable instanceof DataMappingException) {
+                  navigateLiveData.postValue(CommonNavigate.SERVER_DATA_ERROR);
+                }
+              }
+          );
+    }
+  }
+
+  @Override
+  public void preOrderConsumed() {
+    navigateLiveData.postValue(PreOrderNavigate.ORDER_APPROVAL);
+  }
+
+  @Override
+  protected void onCleared() {
+    super.onCleared();
+    disposable.dispose();
+  }
+}
---
+++ app/src/test/java/com/cargopull/executor_driver/presentation/preorder/PreOrderViewModelTest.java
@@ -0,0 +1,220 @@
+package com.cargopull.executor_driver.presentation.preorder;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import android.arch.core.executor.testing.InstantTaskExecutorRule;
+import android.arch.lifecycle.Observer;
+import com.cargopull.executor_driver.ViewModelThreadTestRule;
+import com.cargopull.executor_driver.entity.Order;
+import com.cargopull.executor_driver.gateway.DataMappingException;
+import com.cargopull.executor_driver.interactor.OrderUseCase;
+import com.cargopull.executor_driver.presentation.CommonNavigate;
+import com.cargopull.executor_driver.presentation.ViewState;
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.subjects.PublishSubject;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.runner.RunWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PreOrderViewModelTest {
+
+  @ClassRule
+  public static final ViewModelThreadTestRule classRule = new ViewModelThreadTestRule();
+  @Rule
+  public TestRule rule = new InstantTaskExecutorRule();
+  private PreOrderViewModel viewModel;
+  @Mock
+  private OrderUseCase orderUseCase;
+  @Mock
+  private Order order;
+  @Mock
+  private Order order1;
+  @Mock
+  private Order order2;
+  private PublishSubject<Order> publishSubject;
+
+  @Mock
+  private Observer<ViewState<PreOrderViewActions>> viewStateObserver;
+  @Mock
+  private Observer<String> navigateObserver;
+
+  @Before
+  public void setUp() {
+    publishSubject = PublishSubject.create();
+    when(orderUseCase.getOrders())
+        .thenReturn(publishSubject.toFlowable(BackpressureStrategy.BUFFER));
+    viewModel = new PreOrderViewModelImpl(orderUseCase);
+  }
+
+  /* Тетсируем работу с юзкейсом заказа. */
+
+  /**
+   * Должен просить юзкейс получать заказы при создании.
+   */
+  @Test
+  public void askUseCaseForOrdersInitially() {
+    // Результат:
+    verify(orderUseCase, only()).getOrders();
+  }
+
+  /**
+   * Не должен просить юзкейс получать заказы на подписках.
+   */
+  @Test
+  public void doNotAskUseCaseForOrdersOnSubscriptions() {
+    // Действие:
+    viewModel.getViewStateLiveData();
+    viewModel.getNavigationLiveData();
+    viewModel.getViewStateLiveData();
+    viewModel.getNavigationLiveData();
+
+    // Результат:
+    verify(orderUseCase, only()).getOrders();
+  }
+
+  /* Тетсируем переключение состояний. */
+
+  /**
+   * Должен вернуть состояние вида недоступности изначально.
+   */
+  @Test
+  public void setUnAvailableViewStateToLiveDataInitially() {
+    // Дано:
+    InOrder inOrder = Mockito.inOrder(viewStateObserver);
+
+    // Действие:
+    viewModel.getViewStateLiveData().observeForever(viewStateObserver);
+
+    // Результат:
+    inOrder.verify(viewStateObserver).onChanged(any(PreOrderViewStateUnAvailable.class));
+    verifyNoMoreInteractions(viewStateObserver);
+  }
+
+  /**
+   * Не должен давать иных состояний вида если была ошибка.
+   */
+  @Test
+  public void doNotSetAnyViewStateToLiveDataForError() {
+    // Дано:
+    viewModel.getViewStateLiveData().observeForever(viewStateObserver);
+
+    // Действие:
+    publishSubject.onError(new Exception());
+
+    // Результат:
+    verify(viewStateObserver, only()).onChanged(any(PreOrderViewStateUnAvailable.class));
+  }
+
+  /**
+   * Должен вернуть состояния вида доступности полученными заказами.
+   */
+  @Test
+  public void setIdleViewStateToLiveData() {
+    // Дано:
+    InOrder inOrder = Mockito.inOrder(viewStateObserver);
+    viewModel.getViewStateLiveData().observeForever(viewStateObserver);
+
+    // Действие:
+    publishSubject.onNext(order);
+    publishSubject.onNext(order1);
+    publishSubject.onNext(order2);
+
+    // Результат:
+    inOrder.verify(viewStateObserver).onChanged(any(PreOrderViewStateUnAvailable.class));
+    inOrder.verify(viewStateObserver, times(3)).onChanged(any(PreOrderViewStateAvailable.class));
+    verifyNoMoreInteractions(viewStateObserver);
+  }
+
+  /**
+   * Должен вернуть состояния вида "недоступности" при завершении.
+   */
+  @Test
+  public void setExpiredViewStateToLiveData() {
+    // Дано:
+    InOrder inOrder = Mockito.inOrder(viewStateObserver);
+    PublishSubject<Order> publishSubject = PublishSubject.create();
+    when(orderUseCase.getOrders()).thenReturn(
+        Observable.create(
+            new ObservableOnSubscribe<Order>() {
+              private boolean run;
+
+              @Override
+              public void subscribe(ObservableEmitter<Order> emitter) {
+                if (!run) {
+                  run = true;
+                  emitter.onNext(order);
+                  emitter.onComplete();
+                } else {
+                  emitter.onNext(order2);
+                }
+              }
+            }
+//        )
+        ).startWith(publishSubject)
+            .toFlowable(BackpressureStrategy.BUFFER)
+    );
+    viewModel = new PreOrderViewModelImpl(orderUseCase);
+    viewModel.getNavigationLiveData().observeForever(navigateObserver);
+    viewModel.getViewStateLiveData().observeForever(viewStateObserver);
+
+    // Действие:
+    publishSubject.onComplete();
+
+    // Результат:
+    inOrder.verify(viewStateObserver).onChanged(any(PreOrderViewStateUnAvailable.class));
+    inOrder.verify(viewStateObserver).onChanged(any(PreOrderViewStateAvailable.class));
+    inOrder.verify(viewStateObserver).onChanged(any(PreOrderViewStateUnAvailable.class));
+    inOrder.verify(viewStateObserver).onChanged(any(PreOrderViewStateAvailable.class));
+    verifyNoMoreInteractions(viewStateObserver);
+  }
+
+  /* Тестируем навигацию. */
+
+  /**
+   * Должен вернуть "перейти к ошибке данных сервера".
+   */
+  @Test
+  public void setNavigateToServerDataError() {
+    // Дано:
+    viewModel.getViewStateLiveData().observeForever(viewStateObserver);
+    viewModel.getNavigationLiveData().observeForever(navigateObserver);
+
+    // Действие:
+    publishSubject.onError(new DataMappingException());
+
+    // Результат:
+    verify(navigateObserver, only()).onChanged(CommonNavigate.SERVER_DATA_ERROR);
+  }
+
+  /**
+   * Должен перейти к подтверждению заказа.
+   */
+  @Test
+  public void setNavigateToCloseForMessageConsumed() {
+    // Дано:
+    viewModel.getViewStateLiveData().observeForever(viewStateObserver);
+    viewModel.getNavigationLiveData().observeForever(navigateObserver);
+
+    // Действие:
+    viewModel.preOrderConsumed();
+
+    // Результат:
+    verify(navigateObserver, only()).onChanged(PreOrderNavigate.ORDER_APPROVAL);
+  }
+}
