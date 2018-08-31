d2f2646d11d9e8a9d695c0b40470318cfc224b74
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-08-31 10:40:43 +0300

    feat: Добавил сообщение юзкейсу заказов о том, что заказ потерял актуальность после успешного принятия или отказа от него + дописал и поправил тесты на это.

--- app/src/main/java/com/cargopull/executor_driver/interactor/OrderConfirmationUseCaseImpl.java
+++ app/src/main/java/com/cargopull/executor_driver/interactor/OrderConfirmationUseCaseImpl.java
@@ -1,31 +1,46 @@
 package com.cargopull.executor_driver.interactor;
 
 import android.support.annotation.NonNull;
+import com.cargopull.executor_driver.entity.Order;
+import com.cargopull.executor_driver.entity.PreOrderExpiredException;
 import io.reactivex.Single;
+import io.reactivex.SingleSource;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 import javax.inject.Inject;
 
 public class OrderConfirmationUseCaseImpl implements OrderConfirmationUseCase {
 
   @NonNull
-  private final OrderUseCase orderGateway;
+  private final OrderUseCase orderUseCase;
   @NonNull
   private final OrderConfirmationGateway orderConfirmationGateway;
 
   @Inject
-  public OrderConfirmationUseCaseImpl(@NonNull OrderUseCase orderGateway,
+  public OrderConfirmationUseCaseImpl(@NonNull OrderUseCase orderUseCase,
       @NonNull OrderConfirmationGateway orderConfirmationGateway) {
-    this.orderGateway = orderGateway;
+    this.orderUseCase = orderUseCase;
     this.orderConfirmationGateway = orderConfirmationGateway;
   }
 
   @NonNull
   @Override
   public Single<String> sendDecision(boolean confirmed) {
-    return orderGateway.getOrders()
-        .firstOrError()
+    return orderUseCase.getOrders()
         .observeOn(Schedulers.single())
-        .flatMap(order -> orderConfirmationGateway.sendDecision(order, confirmed))
-        .observeOn(Schedulers.single());
+        .flatMapSingle(new Function<Order, SingleSource<? extends String>>() {
+          boolean orderExpired;
+
+          @Override
+          public SingleSource<? extends String> apply(Order order) throws Exception {
+            if (orderExpired) {
+              throw new PreOrderExpiredException();
+            }
+            orderExpired = true;
+            return orderConfirmationGateway.sendDecision(order, confirmed);
+          }
+        }).observeOn(Schedulers.single())
+        .firstOrError()
+        .doOnSuccess(s -> orderUseCase.setOrderExpired());
   }
 }
--- app/src/test/java/com/cargopull/executor_driver/interactor/OrderConfirmationUseCaseTest.java
+++ app/src/test/java/com/cargopull/executor_driver/interactor/OrderConfirmationUseCaseTest.java
@@ -11,15 +11,19 @@ import static org.mockito.Mockito.when;
 import com.cargopull.executor_driver.UseCaseThreadTestRule;
 import com.cargopull.executor_driver.backend.web.NoNetworkException;
 import com.cargopull.executor_driver.entity.Order;
+import com.cargopull.executor_driver.entity.PreOrderExpiredException;
 import com.cargopull.executor_driver.gateway.DataMappingException;
 import io.reactivex.Flowable;
 import io.reactivex.Single;
+import io.reactivex.functions.Action;
 import io.reactivex.observers.TestObserver;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -38,6 +42,8 @@ public class OrderConfirmationUseCaseTest {
   private Order order;
   @Mock
   private Order order2;
+  @Mock
+  private Action action;
 
   @Before
   public void setUp() {
@@ -49,16 +55,38 @@ public class OrderConfirmationUseCaseTest {
   /* Проверяем работу с юзкейсом заказа */
 
   /**
-   * Должен запросить у юзкейсом получение заказов.
+   * Должен запросить у юзкейса заказа получение заказов.
    */
   @Test
-  public void askGatewayForOrders() {
+  public void askOrderUseCaseForOrders() {
     // Действие:
     useCase.sendDecision(true).test();
     useCase.sendDecision(false).test();
 
     // Результат:
     verify(orderUseCase, times(2)).getOrders();
+    verifyNoMoreInteractions(orderUseCase);
+  }
+
+  /**
+   * Должен запросить у юзкейса заказа деактуализацию заказов.
+   */
+  @Test
+  public void askOrderUseCaseToSetCurrentOrderExpired() {
+    // Дано:
+    InOrder inOrder = Mockito.inOrder(orderUseCase);
+    when(orderUseCase.getOrders()).thenReturn(Flowable.just(order).concatWith(Flowable.never()));
+    when(orderConfirmationGateway.sendDecision(any(), anyBoolean())).thenReturn(Single.just(""));
+
+    // Действие:
+    useCase.sendDecision(true).test();
+    useCase.sendDecision(false).test();
+
+    // Результат:
+    inOrder.verify(orderUseCase).getOrders();
+    inOrder.verify(orderUseCase).setOrderExpired();
+    inOrder.verify(orderUseCase).getOrders();
+    inOrder.verify(orderUseCase).setOrderExpired();
     verifyNoMoreInteractions(orderUseCase);
   }
 
@@ -70,7 +98,7 @@ public class OrderConfirmationUseCaseTest {
   @Test
   public void askGatewayToSendDecisionsForOrders() {
     // Дано:
-    when(orderUseCase.getOrders()).thenReturn(Flowable.just(order));
+    when(orderUseCase.getOrders()).thenReturn(Flowable.just(order).concatWith(Flowable.never()));
 
     // Действие:
     useCase.sendDecision(true).test();
@@ -88,7 +116,8 @@ public class OrderConfirmationUseCaseTest {
   @Test
   public void askGatewayToSendDecisionsForLastOrderOnly() {
     // Дано:
-    when(orderUseCase.getOrders()).thenReturn(Flowable.just(order, order2));
+    when(orderUseCase.getOrders())
+        .thenReturn(Flowable.just(order, order2).concatWith(Flowable.never()));
 
     // Действие:
     useCase.sendDecision(true).test();
@@ -99,13 +128,37 @@ public class OrderConfirmationUseCaseTest {
     verifyNoMoreInteractions(orderConfirmationGateway);
   }
 
+  /**
+   * Должен отменить запрос у гейтвея на передачу решений если пришел новый заказ.
+   */
+  @Test
+  public void cancelGatewayToSendDecisionsForLastOrderOnly() throws Exception {
+    // Дано:
+    InOrder inOrder = Mockito.inOrder(orderConfirmationGateway, action);
+    when(orderUseCase.getOrders())
+        .thenReturn(Flowable.just(order, order2).concatWith(Flowable.never()));
+    when(orderConfirmationGateway.sendDecision(any(), anyBoolean()))
+        .thenReturn(Single.<String>never().doOnDispose(action));
+
+    // Действие:
+    useCase.sendDecision(true).test();
+    useCase.sendDecision(false).test();
+
+    // Результат:
+    inOrder.verify(orderConfirmationGateway).sendDecision(order, true);
+    inOrder.verify(action).run();
+    inOrder.verify(orderConfirmationGateway).sendDecision(order, false);
+    inOrder.verify(action).run();
+    verifyNoMoreInteractions(orderConfirmationGateway, action);
+  }
+
   /* Проверяем ответы на запрос отправки решения */
 
   /**
    * Должен ответить ошибкой маппинга на подтверждение.
    */
   @Test
-  public void answerNoOrdersErrorForAccept() {
+  public void answerDataMappingErrorForAccept() {
     // Дано:
     when(orderUseCase.getOrders()).thenReturn(Flowable.error(new DataMappingException()));
 
@@ -119,12 +172,30 @@ public class OrderConfirmationUseCaseTest {
   }
 
   /**
+   * Должен ответить ошибкой не актуальности заказа на подтверждение.
+   */
+  @Test
+  public void answerOrderExpiredErrorForAccept() {
+    // Дано:
+    when(orderUseCase.getOrders())
+        .thenReturn(Flowable.just(order, order2).concatWith(Flowable.never()));
+
+    // Действие:
+    TestObserver<String> test = useCase.sendDecision(true).test();
+
+    // Результат:
+    test.assertError(PreOrderExpiredException.class);
+    test.assertNoValues();
+    test.assertNotComplete();
+  }
+
+  /**
    * Должен ответить ошибкой сети на подтверждение.
    */
   @Test
   public void answerNoNetworkErrorForAccept() {
     // Дано:
-    when(orderUseCase.getOrders()).thenReturn(Flowable.just(order));
+    when(orderUseCase.getOrders()).thenReturn(Flowable.just(order).concatWith(Flowable.never()));
     when(orderConfirmationGateway.sendDecision(any(), anyBoolean()))
         .thenReturn(Single.error(new NoNetworkException()));
 
@@ -143,7 +214,7 @@ public class OrderConfirmationUseCaseTest {
   @Test
   public void answerNoNetworkErrorForDecline() {
     // Дано:
-    when(orderUseCase.getOrders()).thenReturn(Flowable.just(order));
+    when(orderUseCase.getOrders()).thenReturn(Flowable.just(order).concatWith(Flowable.never()));
     when(orderConfirmationGateway.sendDecision(any(), anyBoolean()))
         .thenReturn(Single.error(new NoNetworkException()));
 
@@ -162,7 +233,7 @@ public class OrderConfirmationUseCaseTest {
   @Test
   public void answerSendAcceptSuccessful() {
     // Дано:
-    when(orderUseCase.getOrders()).thenReturn(Flowable.just(order));
+    when(orderUseCase.getOrders()).thenReturn(Flowable.just(order).concatWith(Flowable.never()));
     when(orderConfirmationGateway.sendDecision(any(), anyBoolean()))
         .thenReturn(Single.just("success"));
 
@@ -181,7 +252,7 @@ public class OrderConfirmationUseCaseTest {
   @Test
   public void answerSendDeclineSuccessful() {
     // Дано:
-    when(orderUseCase.getOrders()).thenReturn(Flowable.just(order));
+    when(orderUseCase.getOrders()).thenReturn(Flowable.just(order).concatWith(Flowable.never()));
     when(orderConfirmationGateway.sendDecision(any(), anyBoolean()))
         .thenReturn(Single.just("success"));
 
@@ -190,7 +261,7 @@ public class OrderConfirmationUseCaseTest {
 
     // Результат:
     test.assertComplete();
-    test.assertComplete();
+    test.assertNoErrors();
     test.assertValue("success");
   }
 }
