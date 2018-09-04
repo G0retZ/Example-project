808adb8ad28e72134094d75f977783d36c4d3fe6
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-09-04 15:44:42 +0300

    feat: выделил интерфейс элемента списка предзаказов, выделил две имплементации с тестами: для заголовков и для карточек.

--- app/src/main/java/com/cargopull/executor_driver/presentation/preorderslist/PreOrdersListItem.java
+++ app/src/main/java/com/cargopull/executor_driver/presentation/preorderslist/PreOrdersListItem.java
@@ -1,91 +1,37 @@
 package com.cargopull.executor_driver.presentation.preorderslist;
 
 import android.content.res.Resources;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import com.cargopull.executor_driver.R;
 import com.cargopull.executor_driver.entity.Order;
-import com.cargopull.executor_driver.entity.RoutePoint;
-import com.cargopull.executor_driver.entity.RoutePointState;
-import java.text.DecimalFormat;
-import java.util.Locale;
-import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
 
-/**
- * Модель для отображения карточки предзаказа в списке предзаказов исполнителя. Тестируемое форматирование.
- */
-public class PreOrdersListItem {
-
-  @NonNull
-  private Order order;
-
-  PreOrdersListItem(@NonNull Order order) {
-    this.order = order;
-  }
-
-  @NonNull
-  public Order getOrder() {
-    return order;
-  }
-
-  public String getOccupationDayOfMonth() {
-    DateTime dateTime = new DateTime(order.getScheduledStartTime());
-    if (dateTime.isBefore(DateTime.now().plusDays(2).withMillisOfDay(0))) {
-      return "";
-    }
-    return DateTimeFormat.forPattern("d").print(dateTime);
-  }
-
-  public String getOccupationMonth(Resources resources) {
-    DateTime dateTime = new DateTime(order.getScheduledStartTime());
-    if (dateTime.isBefore(DateTime.now().plusDays(1).withMillisOfDay(0))) {
-      return resources.getString(R.string.today);
-    } else if (dateTime.isBefore(DateTime.now().plusDays(2).withMillisOfDay(0))) {
-      return resources.getString(R.string.tomorrow);
-    }
-    return DateTimeFormat.forPattern("MMMM").print(dateTime);
-  }
-
-  public String getOccupationDayOfWeek() {
-    return DateTimeFormat.forPattern("EEEE").print(order.getScheduledStartTime());
-  }
-
-  public String getOccupationTime() {
-    long scheduledDate = order.getScheduledStartTime();
-    return
-        DateTimeFormat.forPattern("HH:mm").print(scheduledDate)
-            + "–"
-            + DateTimeFormat.forPattern("HH:mm").print(scheduledDate + order.getEstimatedTime());
-  }
-
-  @NonNull
-  public String getNextAddress() {
-    RoutePoint routePoint = getFirstActiveRoutePoint();
-    return routePoint == null ? "" : routePoint.getAddress().trim();
-  }
-
-  public String getRouteLength() {
-    return String.format(Locale.getDefault(), "%.2f", order.getEstimatedRouteLength() / 1000d);
-  }
-
-  public String getEstimatedPrice(Resources resources) {
-    long currentCost = order.getEstimatedPrice();
-    if (!resources.getBoolean(R.bool.show_cents)) {
-      currentCost = Math.round(currentCost / 100f);
-    }
-    DecimalFormat decimalFormat = new DecimalFormat(resources.getString(R.string.currency_format));
-    decimalFormat.setMaximumFractionDigits(0);
-    return decimalFormat.format(currentCost);
-  }
+interface PreOrdersListItem {
 
   @Nullable
-  private RoutePoint getFirstActiveRoutePoint() {
-    for (RoutePoint routePoint : order.getRoutePath()) {
-      if (routePoint.getRoutePointState() == RoutePointState.ACTIVE) {
-        return routePoint;
-      }
-    }
-    return order.getRoutePath().get(0);
-  }
+  Order getOrder();
+
+  @LayoutRes
+  int getLayoutType();
+
+  @NonNull
+  String getOccupationDayOfMonth();
+
+  @NonNull
+  String getOccupationMonth(@NonNull Resources resources);
+
+  @NonNull
+  String getOccupationDayOfWeek();
+
+  @NonNull
+  String getOccupationTime();
+
+  @NonNull
+  String getNextAddress();
+
+  @NonNull
+  String getRouteLength();
+
+  @NonNull
+  String getEstimatedPrice(@NonNull Resources resources);
 }
--- app/src/test/java/com/cargopull/executor_driver/presentation/preorderslist/PreOrdersListHeaderItemTest.java
+++ app/src/test/java/com/cargopull/executor_driver/presentation/preorderslist/PreOrdersListHeaderItemTest.java
@@ -1,16 +1,11 @@
 package com.cargopull.executor_driver.presentation.preorderslist;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.mockito.Mockito.when;
 
 import android.content.res.Resources;
 import com.cargopull.executor_driver.R;
-import com.cargopull.executor_driver.entity.Order;
-import com.cargopull.executor_driver.entity.RoutePoint;
-import com.cargopull.executor_driver.entity.RoutePointState;
-import java.text.DecimalFormat;
-import java.util.Arrays;
-import java.util.Locale;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.junit.Before;
@@ -20,106 +15,68 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
-public class PreOrdersListItemTest {
+public class PreOrdersListHeaderItemTest {
 
   private PreOrdersListItem preOrdersListItem;
 
-  @Mock
-  private Order order;
-  @Mock
-  private RoutePoint routePoint;
-  @Mock
-  private RoutePoint routePoint1;
-  @Mock
-  private RoutePoint routePoint2;
   @Mock
   private Resources resources;
 
   @Before
   public void setUp() {
-    preOrdersListItem = new PreOrdersListItem(order);
-    when(order.getRoutePath()).thenReturn(Arrays.asList(routePoint, routePoint1, routePoint2));
+    preOrdersListItem = new PreOrdersListHeaderItem(0);
+  }
+
+  @Test
+  public void testGetOrder() {
+    // Результат:
+    assertNull(preOrdersListItem.getOrder());
+  }
+
+  @Test
+  public void testGetLayoutType() {
+    // Результат:
+    assertEquals(preOrdersListItem.getLayoutType(), R.layout.fragment_preorders_list_header);
   }
 
   @Test
   public void testGetNextAddressIfAllQueued() {
-    // Дано:
-    when(routePoint.getRoutePointState()).thenReturn(RoutePointState.QUEUED);
-    when(routePoint1.getRoutePointState()).thenReturn(RoutePointState.QUEUED);
-    when(routePoint2.getRoutePointState()).thenReturn(RoutePointState.QUEUED);
-    when(routePoint.getAddress()).thenReturn("add0");
-
     // Результат:
-    assertEquals(preOrdersListItem.getNextAddress(), "add0");
+    assertEquals(preOrdersListItem.getNextAddress(), "");
   }
 
   @Test
   public void testGetNextAddressIfAllClosed() {
-    // Дано:
-    when(routePoint.getRoutePointState()).thenReturn(RoutePointState.PROCESSED);
-    when(routePoint1.getRoutePointState()).thenReturn(RoutePointState.PROCESSED);
-    when(routePoint2.getRoutePointState()).thenReturn(RoutePointState.PROCESSED);
-    when(routePoint.getAddress()).thenReturn("add0");
-
     // Результат:
-    assertEquals(preOrdersListItem.getNextAddress(), "add0");
+    assertEquals(preOrdersListItem.getNextAddress(), "");
   }
 
   @Test
   public void testGetNextAddressIfSecondActive() {
-    // Дано:
-    when(routePoint.getRoutePointState()).thenReturn(RoutePointState.QUEUED);
-    when(routePoint1.getRoutePointState()).thenReturn(RoutePointState.ACTIVE);
-    when(routePoint1.getAddress()).thenReturn("add1");
-
     // Результат:
-    assertEquals(preOrdersListItem.getNextAddress(), "add1");
+    assertEquals(preOrdersListItem.getNextAddress(), "");
   }
 
   @Test
   public void testGetEstimatedPrice() {
-    // Дано:
-    when(resources.getString(R.string.currency_format)).thenReturn("##,###,### ¤");
-    when(order.getEstimatedPrice()).thenReturn(700000L);
-
-    // Результат:
-    DecimalFormat decimalFormat = new DecimalFormat("##,###,### ¤");
-    decimalFormat.setMaximumFractionDigits(0);
-    assertEquals(preOrdersListItem.getEstimatedPrice(resources), decimalFormat.format(7000));
+    assertEquals(preOrdersListItem.getEstimatedPrice(resources), "");
   }
 
   @Test
   public void testGetRouteLength() {
-    // Дано:
-    when(order.getEstimatedRouteLength()).thenReturn(33239L);
-
     // Результат:
-    assertEquals(preOrdersListItem.getRouteLength(),
-        String.format(Locale.getDefault(), "%.2f", 33239 / 1000d));
+    assertEquals(preOrdersListItem.getRouteLength(), "");
   }
 
   @Test
   public void testGetOccupationTime() {
-    // Дано:
-    when(order.getScheduledStartTime()).thenReturn(22792192L);
-    when(order.getEstimatedTime()).thenReturn(3324339L);
-
-    // Результат:
-    System.out.println(DateTimeFormat.forPattern("HH:mm").print(22792192L)
-        + "–"
-        + DateTimeFormat.forPattern("HH:mm").print(26116531L));
-    assertEquals(preOrdersListItem.getOccupationTime(),
-        DateTimeFormat.forPattern("HH:mm").print(22792192L)
-            + "–"
-            + DateTimeFormat.forPattern("HH:mm").print(26116531L));
+    assertEquals(preOrdersListItem.getOccupationTime(), "");
   }
 
   @Test
   public void testGetOccupationDayOfMonthToday() {
     // Дано:
-    when(order.getScheduledStartTime()).thenReturn(
-        DateTime.now().plusDays(1).withMillisOfDay(0).minusMillis(1).getMillis()
-    );
+    preOrdersListItem = new PreOrdersListHeaderItem(0);
 
     // Результат:
     System.out.println(DateTimeFormat.forPattern("d HH:mm")
@@ -130,9 +87,7 @@ public class PreOrdersListItemTest {
   @Test
   public void testGetOccupationDayOfMonthTomorrow() {
     // Дано:
-    when(order.getScheduledStartTime()).thenReturn(
-        DateTime.now().plusDays(2).withMillisOfDay(0).minusMillis(1).getMillis()
-    );
+    preOrdersListItem = new PreOrdersListHeaderItem(1);
 
     // Результат:
     System.out.println(DateTimeFormat.forPattern("d HH:mm")
@@ -143,9 +98,7 @@ public class PreOrdersListItemTest {
   @Test
   public void testGetOccupationDayOfMonthDayAfterTomorrow() {
     // Дано:
-    when(order.getScheduledStartTime()).thenReturn(
-        DateTime.now().plusDays(2).withMillisOfDay(0).getMillis()
-    );
+    preOrdersListItem = new PreOrdersListHeaderItem(2);
 
     // Результат:
     System.out.println(DateTimeFormat.forPattern("d HH:mm")
@@ -162,9 +115,7 @@ public class PreOrdersListItemTest {
   public void testGetOccupationMonthToday() {
     // Дано:
     when(resources.getString(R.string.today)).thenReturn("today 1");
-    when(order.getScheduledStartTime()).thenReturn(
-        DateTime.now().plusDays(1).withMillisOfDay(0).minusMillis(1).getMillis()
-    );
+    preOrdersListItem = new PreOrdersListHeaderItem(0);
 
     // Результат:
     System.out.println(DateTimeFormat.forPattern("MMMM HH:mm").print(
@@ -177,9 +128,7 @@ public class PreOrdersListItemTest {
   public void testGetOccupationMonthTomorrow() {
     // Дано:
     when(resources.getString(R.string.tomorrow)).thenReturn("tomorrow 1");
-    when(order.getScheduledStartTime()).thenReturn(
-        DateTime.now().plusDays(2).withMillisOfDay(0).minusMillis(1).getMillis()
-    );
+    preOrdersListItem = new PreOrdersListHeaderItem(1);
 
     // Результат:
     System.out.println(DateTimeFormat.forPattern("MMMM HH:mm").print(
@@ -191,9 +140,7 @@ public class PreOrdersListItemTest {
   @Test
   public void testGetOccupationMonthDayAfterTomorrow() {
     // Дано:
-    when(order.getScheduledStartTime()).thenReturn(
-        DateTime.now().plusDays(2).withMillisOfDay(0).getMillis()
-    );
+    preOrdersListItem = new PreOrdersListHeaderItem(2);
 
     // Результат:
     System.out.println(DateTimeFormat.forPattern("MMMM HH:mm").print(
@@ -210,9 +157,7 @@ public class PreOrdersListItemTest {
   @Test
   public void testGetOccupationDayOfWeekToday() {
     // Дано:
-    when(order.getScheduledStartTime()).thenReturn(
-        DateTime.now().plusDays(1).withMillisOfDay(0).minusMillis(1).getMillis()
-    );
+    preOrdersListItem = new PreOrdersListHeaderItem(0);
 
     // Результат:
     System.out.println(DateTimeFormat.forPattern("EEEE HH:mm").print(
@@ -229,9 +174,7 @@ public class PreOrdersListItemTest {
   @Test
   public void testGetOccupationDayOfWeekTomorrow() {
     // Дано:
-    when(order.getScheduledStartTime()).thenReturn(
-        DateTime.now().plusDays(2).withMillisOfDay(0).minusMillis(1).getMillis()
-    );
+    preOrdersListItem = new PreOrdersListHeaderItem(1);
 
     // Результат:
     System.out.println(DateTimeFormat.forPattern("EEEE HH:mm").print(
@@ -248,9 +191,7 @@ public class PreOrdersListItemTest {
   @Test
   public void testGetOccupationDayOfWeekDayAfterTomorrow() {
     // Дано:
-    when(order.getScheduledStartTime()).thenReturn(
-        DateTime.now().plusDays(2).withMillisOfDay(0).getMillis()
-    );
+    preOrdersListItem = new PreOrdersListHeaderItem(2);
 
     // Результат:
     System.out.println(DateTimeFormat.forPattern("EEEE HH:mm").print(
