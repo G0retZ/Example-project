a5e235131a7510ccadb6c3350b013240518e3548
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-09-06 01:00:49 +0300

    feat: навигация к активити выбранного заказа.

--- app/src/main/java/com/cargopull/executor_driver/application/PreOrdersActivity.java
+++ app/src/main/java/com/cargopull/executor_driver/application/PreOrdersActivity.java
@@ -1,9 +1,12 @@
 package com.cargopull.executor_driver.application;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.Toolbar;
 import com.cargopull.executor_driver.R;
+import com.cargopull.executor_driver.presentation.preorderslist.PreOrdersListNavigate;
 
 public class PreOrdersActivity extends BaseActivity {
 
@@ -14,6 +17,17 @@ public class PreOrdersActivity extends BaseActivity {
     Toolbar toolbar = findViewById(R.id.appBar);
     if (toolbar != null) {
       toolbar.setNavigationOnClickListener(v -> onBackPressed());
+    }
+  }
+
+  @Override
+  public void navigate(@NonNull String destination) {
+    switch (destination) {
+      case PreOrdersListNavigate.PRE_ORDER:
+        startActivity(new Intent(this, SelectedPreOrderActivity.class));
+        break;
+      default:
+        super.navigate(destination);
     }
   }
 }
