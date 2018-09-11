acddceb9c2139299d1b43faa5e92484aad977d54
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-09-11 16:34:47 +0300

    fix: поправил маппер элементов списка предзаказов, чтобы учитывал промежутки более 1 дня и временные зоны + попрпавил и дописал тесты на это.

--- app/src/main/java/com/cargopull/executor_driver/presentation/preorderslist/PreOrdersListItemsMapper.java
+++ app/src/main/java/com/cargopull/executor_driver/presentation/preorderslist/PreOrdersListItemsMapper.java
@@ -7,8 +7,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import javax.inject.Inject;
-import org.joda.time.DateTime;
 import org.joda.time.Days;
+import org.joda.time.LocalDate;
 
 /**
  * Маппер списка предзаказов в сортированный список моделей.
@@ -21,23 +21,34 @@ public class PreOrdersListItemsMapper implements Function<Set<Order>, List<PreOr
 
   @Override
   public List<PreOrdersListItem> apply(Set<Order> orders) {
-    DateTime today = DateTime.now().withTimeAtStartOfDay();
     List<PreOrdersListItem> preOrdersListItems = new ArrayList<>();
     ArrayList<Order> ordersList = new ArrayList<>(orders);
     Collections.sort(ordersList,
         (o1, o2) -> Long.compare(o1.getScheduledStartTime(), o2.getScheduledStartTime()));
-    int offset = Days.daysBetween(
-        today,
-        new DateTime(ordersList.get(0).getScheduledStartTime()).withTimeAtStartOfDay()
-    ).getDays() - 1;
-    for (Order order : ordersList) {
-      DateTime preOrderStartDate = new DateTime(order.getScheduledStartTime())
-          .withTimeAtStartOfDay();
-      if (preOrderStartDate.compareTo(today.plusDays(offset)) > 0) {
-        offset++;
-        preOrdersListItems.add(new PreOrdersListHeaderItem(offset));
+    // Устанавливаем первый заголовок
+    preOrdersListItems.add(new PreOrdersListHeaderItem(
+        Days.daysBetween(
+            LocalDate.now(),
+            new LocalDate(ordersList.get(0).getScheduledStartTime())
+        ).getDays()
+    ));
+    for (int i = 0; i < ordersList.size(); i++) {
+      // Если это второй и более элемент
+      if (i > 0) {
+        LocalDate preOrderStartDate0 = new LocalDate(ordersList.get(i - 1).getScheduledStartTime());
+        LocalDate preOrderStartDate1 = new LocalDate(ordersList.get(i).getScheduledStartTime());
+        // Количество дней между предыдущим предзаказом и текущим предзаказом
+        int difference = Days.daysBetween(preOrderStartDate0, preOrderStartDate1).getDays();
+        // Если разница больше 0
+        if (difference > 0) {
+          // Добавляем заголовок со сдвигом относительно сейчас
+          preOrdersListItems.add(new PreOrdersListHeaderItem(
+              Days.daysBetween(LocalDate.now(), preOrderStartDate1).getDays()
+          ));
+        }
       }
-      preOrdersListItems.add(new PreOrdersListOrderItem(order));
+      // Добавляем элемент в список
+      preOrdersListItems.add(new PreOrdersListOrderItem(ordersList.get(i)));
     }
     return preOrdersListItems;
   }
