cb3dfe20c2342783bf067ee5dcf002e47de05999
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-09-14 14:51:55 +0300

    feat: юзкейс отмененного заказа с тестами.

---
+++ app/src/main/java/com/cargopull/executor_driver/interactor/CancelledOrderUseCaseImpl.java
@@ -0,0 +1,38 @@
+package com.cargopull.executor_driver.interactor;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import com.cargopull.executor_driver.entity.Order;
+import com.cargopull.executor_driver.utils.ErrorReporter;
+import io.reactivex.Flowable;
+import io.reactivex.schedulers.Schedulers;
+import javax.inject.Inject;
+
+public class CancelledOrderUseCaseImpl implements OrderUseCase {
+
+  @NonNull
+  private final ErrorReporter errorReporter;
+  @NonNull
+  private final CommonGateway<Order> gateway;
+  @Nullable
+  private Flowable<Order> orderFlowable;
+
+  @Inject
+  public CancelledOrderUseCaseImpl(@NonNull ErrorReporter errorReporter,
+      @NonNull CommonGateway<Order> gateway) {
+    this.errorReporter = errorReporter;
+    this.gateway = gateway;
+  }
+
+  @NonNull
+  @Override
+  public Flowable<Order> getOrders() {
+    if (orderFlowable == null) {
+      orderFlowable = gateway.getData()
+          .observeOn(Schedulers.single())
+          .doOnError(errorReporter::reportError)
+          .share();
+    }
+    return orderFlowable;
+  }
+}
