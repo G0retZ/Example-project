4822de712f590a9fd6ed7b10cb34244bbe6c0de2
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-09-14 18:04:27 +0300

    feat: Объект ошибки отмененного заказа.

---
+++ app/src/main/java/com/cargopull/executor_driver/entity/OrderCancelledException.java
@@ -0,0 +1,11 @@
+package com.cargopull.executor_driver.entity;
+
+/**
+ * Ошибка: заказ был отменен.
+ */
+public class OrderCancelledException extends Exception {
+
+  public OrderCancelledException() {
+    super();
+  }
+}
