0c804a3b2193eef8914922d67dd086f864b1acf8
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-09-21 11:24:47 +0300

    refactor: Поменял UI фрагментов онлайн/не-онлайн для карты и для меню согласно дизайну.

--- app/src/main/java/com/cargopull/executor_driver/view/OnlineFragmentForMenu.java
+++ app/src/main/java/com/cargopull/executor_driver/view/OnlineFragmentForMenu.java
@@ -1,9 +1,45 @@
 package com.cargopull.executor_driver.view;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import com.cargopull.executor_driver.R;
+
 public class OnlineFragmentForMenu extends OnlineFragment {
 
+  private Button takeBreakAction;
+  private Button resumeWorkAction;
+
+  @Nullable
   @Override
-  public void showBreakText(boolean show) {
-    super.showBreakText(false);
+  public View onCreateView(@NonNull LayoutInflater inflater,
+      @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.fragment_online_menu, container, false);
+    takeBreakAction = view.findViewById(R.id.takeBreak);
+    resumeWorkAction = view.findViewById(R.id.resumeWork);
+    return view;
+  }
+
+  @Nullable
+  @Override
+  protected View getBreakText() {
+    return null;
+  }
+
+  @Nullable
+  @Override
+  protected View getTakeBreakAction() {
+    return takeBreakAction;
+  }
+
+  @Nullable
+  @Override
+  protected View getResumeWorkAction() {
+    return resumeWorkAction;
   }
 }
