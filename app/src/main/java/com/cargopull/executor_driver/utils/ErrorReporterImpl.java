78580e90bc6c8cee13910ff7aab2debb924f0162
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-09-26 17:10:18 +0300

    feat: Не отправляем репорты по ошибкам ответа от сервера.

--- app/src/main/java/com/cargopull/executor_driver/utils/ErrorReporterImpl.java
+++ app/src/main/java/com/cargopull/executor_driver/utils/ErrorReporterImpl.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import com.cargopull.executor_driver.BuildConfig;
 import com.cargopull.executor_driver.backend.web.AuthorizationException;
 import com.cargopull.executor_driver.backend.web.NoNetworkException;
+import com.cargopull.executor_driver.backend.web.ServerResponseException;
 import com.cargopull.executor_driver.backend.websocket.ConnectionClosedException;
 import com.cargopull.executor_driver.entity.OrderOfferDecisionException;
 import com.cargopull.executor_driver.entity.OrderOfferExpiredException;
@@ -28,7 +29,9 @@ public class ErrorReporterImpl implements ErrorReporter {
       Log.w(getClass().getSimpleName(), throwable);
     }
     // Игнорируем сетевые ошибки
-    if (throwable instanceof AuthorizationException || throwable instanceof NoNetworkException
+    if (throwable instanceof AuthorizationException
+        || throwable instanceof ServerResponseException
+        || throwable instanceof NoNetworkException
         || throwable instanceof ConnectionClosedException
         || throwable instanceof OrderOfferExpiredException
         || throwable instanceof OrderOfferDecisionException) {
