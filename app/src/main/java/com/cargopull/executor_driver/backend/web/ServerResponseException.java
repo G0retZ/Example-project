ccb5c9a3dc3a63df0e545a926b9daecb2711cfcb
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-09-26 17:01:08 +0300

    feat: Исключение для представления ошибок сервера.

---
+++ app/src/main/java/com/cargopull/executor_driver/backend/web/ServerResponseException.java
@@ -0,0 +1,25 @@
+package com.cargopull.executor_driver.backend.web;
+
+import android.support.annotation.NonNull;
+import java.io.IOException;
+
+/**
+ * Исключение об ошибке сервера с кодом ошибки. Для последующего преобразования кода ошибки в
+ * конкретный {@link Exception} для бизнес слоя.
+ */
+
+public class ServerResponseException extends IOException {
+
+  @NonNull
+  private final String code;
+
+  ServerResponseException(@NonNull String code, @NonNull String message) {
+    super(message);
+    this.code = code;
+  }
+
+  @NonNull
+  public String getCode() {
+    return code;
+  }
+}
