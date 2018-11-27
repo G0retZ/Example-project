4ee64b00e9066d8b368c4e524549d0fe83b6f507
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2018-11-27 13:36:04 +0300

    fix: Поправил состояния вида заголовка истории заказов с тестами для верного отображения.

--- app/src/main/java/com/cargopull/executor_driver/presentation/ordershistoryheader/OrdersHistoryHeaderViewStatePending.java
+++ app/src/main/java/com/cargopull/executor_driver/presentation/ordershistoryheader/OrdersHistoryHeaderViewStatePending.java
@@ -14,12 +14,20 @@ final class OrdersHistoryHeaderViewStatePending implements ViewState<OrdersHisto
     stateActions.setVisible(R.id.pendingIndicator, true);
     stateActions.setVisible(R.id.networkErrorText, false);
     stateActions.setVisible(R.id.retryButton, false);
+    stateActions.setVisible(R.id.summaryTitle, true);
     stateActions.setVisible(R.id.summaryProfit, false);
+    stateActions.setVisible(R.id.slash, true);
     stateActions.setVisible(R.id.summaryLoss, false);
+    stateActions.setVisible(R.id.expandMore, true);
+    stateActions.setVisible(R.id.completedTitle, false);
     stateActions.setVisible(R.id.completed, false);
+    stateActions.setVisible(R.id.commissionTitle, false);
     stateActions.setVisible(R.id.commission, false);
+    stateActions.setVisible(R.id.rejectedTitle, false);
     stateActions.setVisible(R.id.rejected, false);
+    stateActions.setVisible(R.id.cancelledTitle, false);
     stateActions.setVisible(R.id.cancelled, false);
+    stateActions.setVisible(R.id.missedTitle, false);
     stateActions.setVisible(R.id.missed, false);
     stateActions.setClickAction(R.id.expandMore, null);
   }
