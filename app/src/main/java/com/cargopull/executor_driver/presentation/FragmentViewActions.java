8a2328757d79ba13204fb9c81271a9164a6e7c43
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2019-01-31 14:13:09 +0300

    refactor: Методы задания бэкграунда.

--- app/src/main/java/com/cargopull/executor_driver/presentation/FragmentViewActions.java
+++ app/src/main/java/com/cargopull/executor_driver/presentation/FragmentViewActions.java
@@ -4,6 +4,7 @@ package com.cargopull.executor_driver.presentation;
  * Действия для смены состояния вида окна с элементами.
  */
 public interface FragmentViewActions extends DialogViewActions, BlockingViewActions,
-    ImageViewActions, TextViewActions, VisibilityViewActions, ClickableViewActions, ResponsiveViewActions {
+    ImageViewActions, TextViewActions, VisibilityViewActions, ClickableViewActions,
+    ResponsiveViewActions, BackgroundViewActions {
 
 }
