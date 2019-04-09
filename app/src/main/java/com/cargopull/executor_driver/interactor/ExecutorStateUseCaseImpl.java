4aab4b8c3a42c50fd1c79ccccec7d48b71a730f6
Sergei Mitrofanov <GOretZ.m@gmail.com>
2019-04-09 18:46:28 +0300

    feat: добавил возможность обновлять текущий статус исполнителя на уровне юзкейсов + тесты.

--- app/src/main/java/com/cargopull/executor_driver/interactor/ExecutorStateUseCaseImpl.java
+++ app/src/main/java/com/cargopull/executor_driver/interactor/ExecutorStateUseCaseImpl.java
@@ -3,16 +3,33 @@ package com.cargopull.executor_driver.interactor;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.cargopull.executor_driver.entity.ExecutorState;
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Emitter;
 import io.reactivex.Flowable;
 import io.reactivex.schedulers.Schedulers;
 import javax.inject.Inject;
 
-public class ExecutorStateUseCaseImpl implements ExecutorStateUseCase {
+public class ExecutorStateUseCaseImpl implements ExecutorStateUseCase,
+    DataUpdateUseCase<ExecutorState> {
 
   @NonNull
   private final CommonGateway<ExecutorState> gateway;
   @Nullable
   private Flowable<ExecutorState> executorStateFlowable;
+  @NonNull
+  private Emitter<ExecutorState> emitter = new Emitter<ExecutorState>() {
+    @Override
+    public void onNext(ExecutorState value) {
+    }
+
+    @Override
+    public void onError(Throwable error) {
+    }
+
+    @Override
+    public void onComplete() {
+    }
+  };
 
   @Inject
   public ExecutorStateUseCaseImpl(@NonNull CommonGateway<ExecutorState> gateway) {
@@ -23,11 +40,18 @@ public class ExecutorStateUseCaseImpl implements ExecutorStateUseCase {
   @Override
   public Flowable<ExecutorState> getExecutorStates() {
     if (executorStateFlowable == null) {
-      executorStateFlowable = gateway.getData()
-          .observeOn(Schedulers.single())
-          .replay(1)
-          .refCount();
+      executorStateFlowable = Flowable.merge(
+          Flowable.create(emitter -> this.emitter = emitter, BackpressureStrategy.BUFFER),
+          gateway.getData()
+              .observeOn(Schedulers.single())
+              .doOnComplete(() -> emitter.onComplete())
+      ).replay(1).refCount();
     }
     return executorStateFlowable;
+  }
+
+  @Override
+  public void updateWith(@NonNull ExecutorState data) {
+    emitter.onNext(data);
   }
 }
