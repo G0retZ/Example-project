4dc71c36a3e98f184ef40a29e99931b71334dbd4
Sergei Mitrofanov <GOretZ.m@gmail.com>
2019-04-15 22:03:56 +0300

    feat: гейтвея сообщения о прибытии к заказчику с тестами.

--- app/src/main/java/com/cargopull/executor_driver/gateway/ReportArrivedGateway.kt
+++ app/src/main/java/com/cargopull/executor_driver/gateway/ReportArrivedGateway.kt
@@ -1,27 +1,29 @@
 package com.cargopull.executor_driver.gateway
 
 import com.cargopull.executor_driver.backend.web.ApiService
+import com.cargopull.executor_driver.backend.web.incoming.ApiRoutePoint
 import com.cargopull.executor_driver.backend.web.incoming.ApiSimpleResult
 import com.cargopull.executor_driver.entity.ExecutorState
+import com.cargopull.executor_driver.entity.RoutePoint
 import com.cargopull.executor_driver.interactor.CommonGatewaySingle
 import com.google.gson.JsonParseException
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import java.util.*
 
-class ConfirmOrderPaymentGateway
+class ReportArrivedGateway
 constructor(private val apiService: ApiService,
-            private val dataMapper: Mapper<ApiSimpleResult<Void>, Pair<ExecutorState, Void?>>
-) : CommonGatewaySingle<Pair<ExecutorState, Void?>> {
+            private val dataMapper: Mapper<ApiSimpleResult<List<ApiRoutePoint>>, Pair<ExecutorState, List<RoutePoint>?>>
+) : CommonGatewaySingle<Pair<ExecutorState, List<RoutePoint>?>> {
 
     /**
      * Запрашивает сервер завершить заказ.
      *
      * @return [Single] результат - данные либо ошибка на сервере.
      */
-    override val data: Single<Pair<ExecutorState, Void?>>
-        get() = apiService.completeOrderPayment(
-                Collections.singletonMap("status", "COMPLETE_PAYMENT_CONFIRMATION")
+    override val data: Single<Pair<ExecutorState, List<RoutePoint>?>>
+        get() = apiService.reportArrived(
+                Collections.singletonMap("status", "DRIVER_ARRIVED")
         ).subscribeOn(Schedulers.io())
                 .map(dataMapper::map)
                 .onErrorResumeNext { throwable ->
