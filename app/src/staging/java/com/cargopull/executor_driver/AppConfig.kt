b06fcb30aa46a909c8167e7ef413c2ccc1be5c36
Sergei Mitrofanov <goretz.m@gmail.com>
2019-05-20 15:38:12 +0300

    fix: Прописал урл staging сервера вместо дев

--- app/src/staging/java/com/cargopull/executor_driver/AppConfig.kt
+++ app/src/staging/java/com/cargopull/executor_driver/AppConfig.kt
@@ -2,8 +2,8 @@ package com.cargopull.executor_driver
 
 const val QUIET_CHANNEL_ID = "state_channel"
 const val ANNOUNCEMENT_CHANNEL_ID = "announcement_channel"
-const val BASE_URL = "http://tst.capsrv.xyz:9080/executor/"
-const val SOCKET_URL = "ws://tst.capsrv.xyz:9080/executor/ws"
+const val BASE_URL = "http://stg.capsrv.xyz:8080/executor/"
+const val SOCKET_URL = "ws://stg.capsrv.xyz:8080/executor/ws"
 const val GEOLOCATION_DESTINATION = "/mobile/online"
 fun STATUS_DESTINATION(value: String) = "/queue/$value"
 const val POLLING_DESTINATION = "/mobile/retrieveOverPackage"
