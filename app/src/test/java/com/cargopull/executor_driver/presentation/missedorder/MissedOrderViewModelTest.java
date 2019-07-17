d0f5b63e501a72f8e48e0dc84b4507d808442f94
Sergey Mitrofanov <s.mitrofanov@fasten.com>
2019-07-17 20:54:25 +0300

    Играть звук и вибро во вьюмоделе пропуска заказа

--- app/src/test/java/com/cargopull/executor_driver/presentation/missedorder/MissedOrderViewModelTest.java
+++ app/src/test/java/com/cargopull/executor_driver/presentation/missedorder/MissedOrderViewModelTest.java
@@ -8,8 +8,11 @@ import static org.mockito.Mockito.when;
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 import androidx.lifecycle.Observer;
+import com.cargopull.executor_driver.R;
 import com.cargopull.executor_driver.ViewModelThreadTestRule;
 import com.cargopull.executor_driver.backend.analytics.ErrorReporter;
+import com.cargopull.executor_driver.backend.ringtone.RingTonePlayer;
+import com.cargopull.executor_driver.backend.vibro.ShakeItPlayer;
 import com.cargopull.executor_driver.gateway.DataMappingException;
 import com.cargopull.executor_driver.interactor.NotificationMessageUseCase;
 import com.cargopull.executor_driver.presentation.ViewState;
@@ -39,6 +42,10 @@ public class MissedOrderViewModelTest {
   @Mock
   private NotificationMessageUseCase useCase;
   @Mock
+  private ShakeItPlayer shakeItPlayer;
+  @Mock
+  private RingTonePlayer ringTonePlayer;
+  @Mock
   private Observer<ViewState<MissedOrderViewActions>> viewStateObserver;
   @Captor
   private ArgumentCaptor<ViewState<MissedOrderViewActions>> viewStateCaptor;
@@ -52,7 +59,7 @@ public class MissedOrderViewModelTest {
     publishSubject = PublishSubject.create();
     when(useCase.getNotificationMessages())
         .thenReturn(publishSubject.toFlowable(BackpressureStrategy.BUFFER));
-    viewModel = new MissedOrderViewModelImpl(errorReporter, useCase);
+    viewModel = new MissedOrderViewModelImpl(errorReporter, useCase, shakeItPlayer, ringTonePlayer);
   }
 
   /* Проверяем отправку ошибок в репортер */
@@ -95,6 +102,76 @@ public class MissedOrderViewModelTest {
     verify(useCase, only()).getNotificationMessages();
   }
 
+  /* Тетсируем работу с вибро и звуком. */
+
+  /**
+   * Не должен трогать вибро и звук изначально и на подписках.
+   */
+  @Test
+  public void doNotTouchVibrationAndSoundInitially() {
+    // Действие:
+    viewModel.getViewStateLiveData();
+    viewModel.getNavigationLiveData();
+    viewModel.getViewStateLiveData();
+    viewModel.getNavigationLiveData();
+
+    // Результат:
+    verifyZeroInteractions(shakeItPlayer);
+    verifyZeroInteractions(ringTonePlayer);
+  }
+
+  /**
+   * Не должен трогать вибро и звук при ошибке.
+   */
+  @Test
+  public void doNotTouchVibrationAndSoundOnError() {
+    // Действие:
+    publishSubject.onError(new DataMappingException());
+
+    // Результат:
+    verifyZeroInteractions(shakeItPlayer);
+    verifyZeroInteractions(ringTonePlayer);
+  }
+
+  /**
+   * Должен дать вибро и звук отказа при получении упущения.
+   */
+  @Test
+  public void useVibrationAndSoundOnOrderMissedWithMessage() {
+    // Действие:
+    publishSubject.onNext("Message");
+
+    // Результат:
+    verify(shakeItPlayer, only()).shakeIt(R.raw.skip_order_vibro);
+    verify(ringTonePlayer, only()).playRingTone(R.raw.skip_order);
+  }
+
+  /**
+   * Должен дать вибро и звук отказа при получении упущения.
+   */
+  @Test
+  public void useVibrationAndSoundOnOrderMissedWithEmptyMessage() {
+    // Действие:
+    publishSubject.onNext("");
+
+    // Результат:
+    verify(shakeItPlayer, only()).shakeIt(R.raw.skip_order_vibro);
+    verify(ringTonePlayer, only()).playRingTone(R.raw.skip_order);
+  }
+
+  /**
+   * Должен дать вибро и звук отказа при получении упущения.
+   */
+  @Test
+  public void useVibrationAndSoundOnOrderMissedWithSpaceMessage() {
+    // Действие:
+    publishSubject.onNext("\n");
+
+    // Результат:
+    verify(shakeItPlayer, only()).shakeIt(R.raw.skip_order_vibro);
+    verify(ringTonePlayer, only()).playRingTone(R.raw.skip_order);
+  }
+
   /* Тетсируем сообщение. */
 
   /**
@@ -112,6 +189,21 @@ public class MissedOrderViewModelTest {
     verify(viewStateObserver, only()).onChanged(viewStateCaptor.capture());
     viewStateCaptor.getValue().apply(viewActions);
     verify(viewActions, only()).showMissedOrderMessage("Message");
+  }
+
+  /**
+   * Не должен показывать сообщение при ошибке.
+   */
+  @Test
+  public void doNotShowMessageOnError() {
+    // Дано:
+    viewModel.getViewStateLiveData().observeForever(viewStateObserver);
+
+    // Действие:
+    publishSubject.onError(new DataMappingException());
+
+    // Результат:
+    verifyZeroInteractions(viewStateObserver);
   }
 
   /**
