e34052f318e3ae3f6c21247e125438e4962df6eb
Sergei Mitrofanov <GOretZ.m@gmail.com>
2019-08-01 07:04:04 +0300

    Выделить имя сервера для проверки подключения

--- app/src/release/java/com/cargopull/executor_driver/AppConfig.kt
+++ app/src/release/java/com/cargopull/executor_driver/AppConfig.kt
@@ -2,8 +2,9 @@ package com.cargopull.executor_driver
 
 const val QUIET_CHANNEL_ID = "state_channel"
 const val ANNOUNCEMENT_CHANNEL_ID = "announcement_channel"
-const val BASE_URL = "https://gate.cargopull.host:8443/executor/"
-const val SOCKET_URL = "wss://gate.cargopull.host:8443/executor/ws"
+const val SERVER_NAME = "gate.cargopull.host"
+const val BASE_URL = "https://$SERVER_NAME:8443/executor/"
+const val SOCKET_URL = "wss://$SERVER_NAME:8443/executor/ws"
 const val GEOLOCATION_DESTINATION = "/mobile/online"
 fun STATUS_DESTINATION(value: String) = "/queue/$value"
 const val POLLING_DESTINATION = "/mobile/retrieveOverPackage"
